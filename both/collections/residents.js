638188582b1df69bfe61b656aca8a5085a81d486
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-06-17 16:29:42 +0300

    Residents helpers.

--- both/collections/residents.js
+++ both/collections/residents.js
@@ -49,3 +49,15 @@ var ResidentsSchema = new SimpleSchema({
 });
 
 Residents.attachSchema(ResidentsSchema);
+
+Residents.helpers({
+  home: function() {
+    return Homes.findOne(this.homeId);
+  },
+  createdUser: function() {
+      return Meteor.users.findOne(this.createdUserId);
+  },
+  updatedUser: function() {
+      return Meteor.users.findOne(this.updatedUserId);
+  }
+});
