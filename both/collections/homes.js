3648ec50e98d02b416ca9b8d02484f61669b6b67
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-06-18 14:40:55 +0300

    Simplify schemas.

--- both/collections/homes.js
+++ both/collections/homes.js
@@ -1,1 +1,10 @@
 Homes = new Mongo.Collection('homes');
+
+var HomesSchema = new SimpleSchema({
+  name:{
+    type:String,
+    label: 'Home Name',
+  }
+});
+
+Homes.attachSchema(HomesSchema);
--- both/collections/residents.js
+++ both/collections/residents.js
@@ -19,42 +19,6 @@ var ResidentsSchema = new SimpleSchema({
         });
       }
     }
-  },
-  createdAt: {
-    type: Date,
-    label: "Created Date",
-    autoValue: function() {
-      if (this.isInsert)
-        return new Date;
-    },
-    denyUpdate: true
-  },
-  updatedAt: {
-    type: Date,
-    label: "Updated Date",
-    autoValue: function() {
-      return new Date();
-    },
-    optional: true
-  },
-  createdUserId: {
-    type: String,
-    label: "Created by",
-    autoValue: function() {
-      if (this.isInsert && !this.value)
-        return this.userId;
-    },
-    denyUpdate: true,
-    optional: true
-  },
-  updatedUserId: {
-    type: String,
-    label: "Updated by",
-    autoValue: function() {
-      if (!this.value)
-        return this.userId;
-    },
-    optional: true
   }
 });
 
