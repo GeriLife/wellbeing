97f8d26d34f22ea546c14a95d52b15be866d2c59
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-06-18 15:07:42 +0300

    Admin configuration object.

---
+++ both/adminConfig.js
@@ -0,0 +1,15 @@
+AdminConfig = {
+  name: "Juhani Wellbeing",
+  adminEmails: ['test@test.com'],
+  collections:
+  {
+    Residents: {
+      icon: "users",
+      color: "green",
+      tableColumns: [
+        {label: "First name", name: "firstName"}
+      ]
+    },
+    Homes: {}
+  }
+}
