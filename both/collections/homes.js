b5d1d6e354da588a87907562489583db061356a1
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-06-25 14:46:48 +0300

    Connected homes to groups.

--- both/collections/homes.js
+++ both/collections/homes.js
@@ -4,6 +4,20 @@ var HomesSchema = new SimpleSchema({
   name:{
     type:String,
     label: 'Home Name',
+  },
+  groupId: {
+    type: String,
+    label: 'Group',
+    autoform: {
+      options: function() {
+        return _.map(Groups.find().fetch(), function(group) {
+          return {
+            label: group.name,
+            value: group._id
+          };
+        });
+      }
+    }
   }
 });
 
