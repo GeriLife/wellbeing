4f37df370228bfb0badaecdac637611123cea189
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-07-01 21:20:50 +0300

    Autoform hook: flash message.

---
+++ client/views/add-activity.js
@@ -0,0 +1,6 @@
+AutoForm.addHooks('add-activity-form', {
+    onSuccess: function(operation, resultId, template) {
+        // Alert user that activity was added
+        FlashMessages.sendSuccess('<i class="fa fa-check"></i> Activity added!');
+    }
+});
