6b86e51f63e5790ef70d6483cce262b767a5f10b
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-07-01 16:41:31 +0300

    Make sure user is logged in.

--- client/routes.js
+++ client/routes.js
@@ -1,3 +1,16 @@
 Router.route('/', function () {
   this.render('main');
+}, {
+  name: 'front'
 });
+
+var requiresUserLogin = function () {
+    if (!Meteor.user()) {
+        this.render('login');
+    } else {
+        this.next();
+    }
+};
+
+// User login required for event registration
+Router.onBeforeAction(requiresUserLogin, {only: ['front']});
