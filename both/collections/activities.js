c84ca0de847704242a7e0fdeeec7a868ff2f5adf
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-07-02 21:30:27 +0300

    Add resident last initial to select option.

--- both/collections/activities.js
+++ both/collections/activities.js
@@ -28,7 +28,11 @@ var ActivitiesSchema = new SimpleSchema({
 
           // Create a residents array with name/ID pairs for label/value
           var residentOptions = _.map(homeResidents, function (homeResident) {
-            return {label: homeResident.firstName, value: homeResident._id};
+            // Combine resident first name and last initial
+            var residentName = homeResident.firstName + ' ' + homeResident.lastInitial;
+
+            // Create option for this resident, with ID as the value
+            return {label: residentName, value: homeResident._id};
           });
 
           // Return residents and home as option group
