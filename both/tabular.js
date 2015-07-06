cf62abceb2ae72123b09a90369878631ab20f81a
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-07-06 21:29:26 +0300

    Residents data table.

--- both/tabular.js
+++ both/tabular.js
@@ -1,3 +1,11 @@
 TabularTables = {};
 
 Meteor.isClient && Template.registerHelper('TabularTables', TabularTables);
+
+TabularTables.Residents = new Tabular.Table({
+  name: "Residents",
+  collection: Residents,
+  columns: [
+    {data: "firstName", title: "First name"}
+  ]
+});
