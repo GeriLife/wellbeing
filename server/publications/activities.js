2963a679e0fe49246f9cc434f61df022bf5f03df
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-07-06 21:18:18 +0300

    userActivities publication.

--- server/publications/activities.js
+++ server/publications/activities.js
@@ -1,3 +1,8 @@
 Meteor.publish('allActivities', function () {
   return Activities.find();
 });
+
+Meteor.publish('userActivities', function (userId) {
+  // Activities for a given user
+  return Activities.find({'createdById': userId});
+});
