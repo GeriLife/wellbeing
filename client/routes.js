d182dd87eb2620fb8947740b71298af8829dadf6
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-07-06 21:25:34 +0300

    Residents route.

--- client/routes.js
+++ client/routes.js
@@ -8,12 +8,24 @@ Router.route('/', function () {
   name: 'front'
 });
 
+Router.route('/residents', function () {
+  this.render('residents');
+}, {
+  name: 'residents'
+});
+
+Router.route('/graph', function () {
+  this.render('graph');
+}, {
+  name: 'graph'
+});
+
 var requiresUserLogin = function () {
-    if (!Meteor.user()) {
-        this.render('login');
-    } else {
-        this.next();
-    }
+  if (!Meteor.user()) {
+    this.render('login');
+  } else {
+    this.next();
+  }
 };
 
 // User login required for event registration
