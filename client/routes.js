3c13efc25f3a1f96e54bd0f32224f6370306ee38
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-07-07 23:30:13 +0300

    Added resident data context.

--- client/routes.js
+++ client/routes.js
@@ -15,7 +15,13 @@ Router.route('/residents', function () {
 });
 
 Router.route('/resident/:residentId', function () {
-  this.render('resident');
+  var controller = this;
+  var residentId = controller.params.residentId;
+  this.render('resident', {
+    data: function() {
+      return Residents.findOne(residentId);
+    }
+  });
 }, {
   name: 'resident'
 });
