41af176ee68dde1e8d771b27135aa45426f7ad0f
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-07-23 18:42:35 +0300

    Added homeActivities publication.

--- server/publications/activities.js
+++ server/publications/activities.js
@@ -11,3 +11,14 @@ Meteor.publish('residentActivities', function (residentId) {
   // Activities for a given user
   return Activities.find({'residentIds':  residentId });
 });
+
+Meteor.publish('homeActivities', function (homeId) {
+  // Get all resident of this home
+  var homeResidentIds = Meteor.call('getHomeResidentIds', homeId);
+
+  // Get an array of all activity Ids for residents of this home
+  var homeResidentActivities = Activities.find({'residentIds': {$elemMatch: {$in: homeResidentIds}}}).fetch();
+
+  // return all activities from activity IDs array
+  return homeResidentActivities;
+});
