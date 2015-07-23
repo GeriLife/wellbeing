71a3010cde4a736e4dd12503d6d22bace3bdf1b9
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-07-23 09:44:27 +0300

    Use summed activities.

--- client/views/resident/activities/chart/chart.js
+++ client/views/resident/activities/chart/chart.js
@@ -10,13 +10,39 @@ Template.residentActivityChart.rendered = function () {
 
   //TODO: Make sure there can be multiple activities in a single day.
 
+  // Create a nest of activities grouped by activity date
+  var nestedActivities = d3.nest()
+    .key(function (activity) {
+      return activity.activityDate;
+    })
+
+  // Get a sum of activities
+  var summedActivities = nestedActivities.rollup(function (activity) {
+      return {
+        duration: d3.sum(activity, function (activity) {
+          return activity.duration;
+        })
+      }
+    })
+    .entries(activities);
+
+    summedActivities.map(function (activity) {
+      // Create date and duration attributes with proper data types
+      activity.date = new Date(activity.key);
+      activity.duration = parseInt(activity.values.duration);
+
+      // Delete unused key and values
+      delete activity.values
+      delete activity.key;
+    })
+
   // Render the chart using MetricsGraphics charting library
   MG.data_graphic({
     title: "Daily activity in minutes.",
     description: "Showing daily activity minutes for " + residentName,
     y_label: 'Minutes',
     y_rug: true,
-    data: activities,
+    data: summedActivities,
     interpolate: 'basic',
     missing_is_zero: true,
     area: false,
@@ -24,7 +50,7 @@ Template.residentActivityChart.rendered = function () {
     height: 200,
     right: 40,
     target: document.getElementById('activity-chart'),
-    x_accessor: 'activityDate',
+    x_accessor: 'date',
     y_accessor: 'duration'
   });
 }
