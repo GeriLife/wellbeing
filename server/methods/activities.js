b817d020c3c7a21ae315bb354fdb51358af4c407
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-17 16:16:50 +0300

    Add get residents without activity by type.

--- server/methods/activities.js
+++ server/methods/activities.js
@@ -18,5 +18,22 @@ Meteor.methods({
     ]);
 
     return residentLastActivityByType;
+  },
+  'getResidentsWithoutActivityByType': function (activityTypeId) {
+    // Get resident latest activity by type
+    var residentLastActivityByType = Meteor.call('getResidentsLatestActivityByType', activityTypeId);
+
+    // Get all resident IDs
+    var residentIds = Residents.find({}, {fields: {_id: 1}}).fetch();
+
+    // Get residents who have participated in the activity type
+    var residentsWithPreviousActivity = _.map(residentLastActivityByType, function (resident) {
+      return resident._id;
+    });
+
+    // Get residents who have not participated in activity type
+    var residentsWithoutPreviousActivity = _.difference(residentIds, residentsWithPreviousActivity);
+
+    return residentsWithoutPreviousActivity;
   }
 });
