fa056e1b57b0ea568dff884be7d64ec0270b70f5
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-17 17:45:23 +0300

    Add selectResidents publication

--- server/publications/residents.js
+++ server/publications/residents.js
@@ -1,3 +1,7 @@
 Meteor.publish('allResidents', function () {
   return Residents.find();
 });
+
+Meteor.publish('selectResidents', function (residentIdsArray) {
+  return Residents.find({_id: {$in: residentIdsArray}});
+});
