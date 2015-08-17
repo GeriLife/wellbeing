c400afd3ebd53a37fb8e13eb39e1dbca49c1d400
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-17 19:19:18 +0300

    Autorun and template helper to render table

--- client/views/activities/latest/table/table.js
+++ client/views/activities/latest/table/table.js
@@ -1,7 +1,16 @@
 Template.latestActivitiesByTypeTable.helpers({
   tableData: function () {
-    // Columns resident name, timeAgo
-    return [];
+    // Get resident activity details from template
+    var residentActivities = instance.residentDetails.get();
+
+    if (residentActivities) {
+      // Render resident details to the table
+      // Columns resident name, timeAgo
+      return residentActivities;
+    } else {
+      // return an empty array to simply render the table
+      return [];
+    }
   }
 });
 
@@ -9,16 +18,42 @@ Template.latestActivitiesByTypeTable.created = function () {
   // Set instance variable for consistency
   instance = Template.instance();
 
+  instance.residentDetails = new ReactiveVar();
+
   instance.autorun(function () {
     // Keep track of the selected activity type
-    var latestResidentActivities = instance.parent().latestResidentsActivityByType.get();
+    var residentActivities = instance.parent().latestResidentsActivityByType.get();
 
-    var residentIds = _.map(latestResidentActivities, function (residentActivity) {
+    var residentIds = _.map(residentActivities, function (residentActivity) {
       // Get the resident ID
       return residentActivity._id;
     });
 
     // Get residents from database
     instance.subscribe('selectResidents', residentIds);
+
+    var residentNamesWithActivityDates = _.map(
+      residentActivities,
+      function (residentActivity) {
+        // Get resident ID from activity
+        var residentId = residentActivity._id;
+
+        // Add activity date in 'time ago' format
+        var activityTimeAgo = new moment(residentActivity.latestActivity).fromNow();
+
+        // Get resident details from collection
+        var resident = Residents.findOne(residentId);
+
+        // Create an object with details
+        var residentDetails = {
+          firstName: resident.firstName,
+          lastName: resident.lastName,
+          lastActive: activityTimeAgo
+        }
+
+        return residentDetails;
+    });
+
+    instance.residentDetails.set(residentNamesWithActivityDates);
   });
 };
