dbfb95de81ef664257f5108cf4e222fb9b034731
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-17 17:06:52 +0300

    Add activityTypeSelection reactive var

--- client/views/activities/latest/latestByType.js
+++ client/views/activities/latest/latestByType.js
@@ -3,7 +3,10 @@ Template.latestActivitiesByType.created = function () {
   var instance = this;
 
   // Subscribe to all activity types
-  instance.subscribe('allActivityTypes'); 
+  instance.subscribe('allActivityTypes');
+
+  // Create variable to hold activity type selection
+  instance.activityTypeSelection = new ReactiveVar();
 };
 
 Template.latestActivitiesByType.helpers({
