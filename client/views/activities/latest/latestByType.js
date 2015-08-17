55c29686ccad9e060b527bc3f5e82ce25879d089
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-17 17:03:11 +0300

    Add activityTypeOptions helper

--- client/views/activities/latest/latestByType.js
+++ client/views/activities/latest/latestByType.js
@@ -2,3 +2,12 @@ Template.latestActivitiesByType.created = function () {
   // Subscribe to all activity types
   this.subscribe('allActivityTypes');
 };
+
+Template.latestActivitiesByType.helpers({
+  'activityTypeOptions': function () {
+    // Get all activity types
+    activityTypes = ActivityTypes.find().fetch();
+
+    return activityTypes;
+  }
+});
