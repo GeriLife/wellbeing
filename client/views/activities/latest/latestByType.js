6dd29a1fd539232b4438de7ac137f055c8aafa3e
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-18 12:14:11 +0300

    Remove unneeded code

--- client/views/activities/latest/latestByType.js
+++ client/views/activities/latest/latestByType.js
@@ -4,37 +4,4 @@ Template.latestActivitiesByType.created = function () {
 
   // Create variable to hold activity type selection
   instance.activityTypeSelection = new ReactiveVar();
-
-  // Create latest resident activity placeholder
-  instance.latestResidentsActivityByType = new ReactiveVar();
-
-  instance.autorun(function () {
-    // Get activity type selection
-    var activityTypeSelection = instance.activityTypeSelection.get();
-
-    if (activityTypeSelection) {
-      // Get latest resident activities for selected activity type
-      Meteor.call(
-        'getResidentsLatestActivityByType',
-        activityTypeSelection,
-        function (error, latestActivity) {
-          if (error) {
-            console.log(error);
-          } else {
-            // Update the reactive variable with latest resident activity
-            instance.latestResidentsActivityByType.set(latestActivity);
-          }
-        });
-    };
-  });
-
-  instance.autorun(function () {
-    var latestResidentActivities = instance.latestResidentsActivityByType.get();
-
-    var residentIds = _.map(latestResidentActivities, function (residentActivity) {
-      return residentActivity._id;
-    });
-
-    instance.subscribe('selectResidents', residentIds);
-  });
 };
