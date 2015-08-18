50e81d7d939fe0a8a643bdeba3fe4bdecc64837a
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-18 12:14:59 +0300

    Get residents latest activities. Cleanup.

--- client/views/activities/latest/table/table.js
+++ client/views/activities/latest/table/table.js
@@ -1,16 +1,9 @@
 Template.latestActivitiesByTypeTable.helpers({
-  tableData: function () {
+  residentsLatestActivities: function () {
     // Get resident activity details from template
-    var residentActivities = instance.residentDetails.get();
+    var residentsLatestActivities = instance.residentLatestActivityDetails.get();
 
-    if (residentActivities) {
-      // Render resident details to the table
-      // Columns resident name, timeAgo
-      return residentActivities;
-    } else {
-      // return an empty array to simply render the table
-      return [];
-    }
+    return residentsLatestActivities;
   }
 });
 
@@ -18,42 +11,23 @@ Template.latestActivitiesByTypeTable.created = function () {
   // Set instance variable for consistency
   instance = Template.instance();
 
-  instance.residentDetails = new ReactiveVar();
+  instance.residentLatestActivityDetails = new ReactiveVar();
 
   instance.autorun(function () {
     // Keep track of the selected activity type
-    var residentActivities = instance.parent().latestResidentsActivityByType.get();
+    var activityTypeId = instance.parent().activityTypeSelection.get();
 
-    var residentIds = _.map(residentActivities, function (residentActivity) {
-      // Get the resident ID
-      return residentActivity._id;
-    });
-
-    // Get residents from database
-    instance.subscribe('selectResidents', residentIds);
-
-    var residentNamesWithActivityDates = _.map(
-      residentActivities,
-      function (residentActivity) {
-        // Get resident ID from activity
-        var residentId = residentActivity._id;
-
-        // Add activity date in 'time ago' format
-        var activityTimeAgo = new moment(residentActivity.latestActivity).fromNow();
-
-        // Get resident details from collection
-        var resident = Residents.findOne(residentId);
-
-        // Create an object with details
-        var residentDetails = {
-          firstName: resident.firstName,
-          lastName: resident.lastName,
-          lastActive: activityTimeAgo
-        };
-
-        return residentDetails;
-    });
-
-    instance.residentDetails.set(residentNamesWithActivityDates);
+    if (activityTypeId) {
+      Meteor.call(
+        'getResidentsNameHomeAndLastActivityByType',
+        activityTypeId,
+        function (error, residentLatestActivity) {
+          if (error) {
+            console.log(error);
+          } else {
+            instance.residentLatestActivityDetails.set(residentLatestActivity);
+          }
+        });
+      }
   });
 };
