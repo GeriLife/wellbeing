80fe6f23970ff9b0a865f31fb8e4ed5666607d10
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-20 11:31:15 +0300

    Add homesBelongingToGroup publication

--- server/publications/homes.js
+++ server/publications/homes.js
@@ -2,3 +2,8 @@ Meteor.publish('allHomes', function () {
   // All homes publication
   return Homes.find();
 });
+
+Meteor.publish('homesBelongingToGroup', function (groupId) {
+  // All homes publication
+  return Homes.find({groupId: groupId});
+});
