dd66f2845b1250d118aa47f215ebdcc2a547ab7c
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-21 02:57:25 +0300

    Residents template helper

--- client/views/residents/residents.js
+++ client/views/residents/residents.js
@@ -1,3 +1,49 @@
 Template.residents.created = function () {
   this.subscribe('residentsHomesComposite');
 };
+
+Template.residents.helpers({
+  'residents': function () {
+    // Get all residents
+    var residents = Residents.find().fetch();
+
+    // Create a placeholder array for resident objects
+    var residentsArray = [];
+
+    // Iterate through residents
+    // set full name and home name from collection helpers
+    residents.forEach(function (resident) {
+      var residentObject = {
+        fullName: resident.fullName(),
+        homeName: resident.homeName()
+      };
+
+      // Add resident object to residents list
+      residentsArray.push(residentObject);
+
+    });
+
+    return residentsArray;
+  },
+  'tableSettings': function () {
+    var tableSettings = {
+      showFilter: false,
+      fields: [
+        {
+          key: 'fullName',
+          label: 'Resident Name',
+          sortOrder: 0,
+          sortDirection: 'ascending'
+        },
+        {
+          key: 'homeName',
+          label: 'Home Name',
+          sortOrder: 1,
+          sortDirection: 'ascending'
+        }
+      ]
+    };
+
+    return tableSettings;
+  }
+});
