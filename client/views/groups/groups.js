681fba921c21f3205b7c1e89166ba0f538e36145
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-20 10:37:46 +0300

    Add allGroups subscription

--- client/views/groups/groups.js
+++ client/views/groups/groups.js
@@ -1,3 +1,9 @@
+Template.groups.created = function () {
+  var instance = this;
+
+  instance.subscribe('allGroups');
+};
+
 Template.groups.helpers({
   'groups': function () {
     return Groups.find();
