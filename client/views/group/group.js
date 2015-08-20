c98f5d56e8c1df596b5a648705dc302a3cb4f7db
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-20 12:11:24 +0300

    Add instance subscriptions, group helper

--- client/views/group/group.js
+++ client/views/group/group.js
@@ -1,7 +1,21 @@
 Template.group.helpers({
+  'group': function () {
+    var instance = Template.instance();
+
+    // Get Group ID from template instance
+    var groupId = instance.groupId;
+
+    // Get group from Groups collection
+    var group = Groups.findOne(groupId);
+
+    return group;
+  },
   'homes': function () {
+    var instance = Template.instance();
+
     // Get homes for this group
-    var groupId = Router.current().params.groupId;
+    var groupId = instance.groupId;
+
     return Homes.find({ "groupId": groupId });
   }
 });
@@ -10,8 +24,11 @@ Template.group.created = function () {
   var instance = this;
 
   // Get group ID
-  var groupId = Router.current().params.groupId;
+  instance.groupId = Router.current().params.groupId;
+
+  // Subscribe to single group
+  instance.subscribe('singleGroup', instance.groupId);
 
   // Subscribe to homes beonging to group
-  instance.subscribe('homesBelongingToGroup', groupId);
+  instance.subscribe('homesBelongingToGroup', instance.groupId);
 };
