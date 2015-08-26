5097929f79cb5774e6a85edda3a07aeb4fbc323d
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-26 18:29:04 +0300

    Add event to update reactive variable

--- client/views/activities/activities.js
+++ client/views/activities/activities.js
@@ -14,3 +14,13 @@ Template.activities.rendered = function () {
   instance.allOrLatestToggle = document.querySelector("[name='all-or-latest']");
   instance.switch = new Switchery(instance.allOrLatestToggle, options);
 };
+
+Template.activities.events({
+  "change [name='all-or-latest']": function (event, template) {
+    // Get reference to template instance
+    var instance = Template.instance();
+
+    // Set reactive variable with value of all-or-latest toggle
+    instance.showLatestActivities.set(instance.allOrLatestToggle.checked);
+  }
+});
