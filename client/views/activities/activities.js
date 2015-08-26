19f9f760ac63ba2ecd1b5719fa5f27d9225c8765
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-26 18:04:08 +0300

    Add showLatestActivities reactive var

--- client/views/activities/activities.js
+++ client/views/activities/activities.js
@@ -1,3 +1,7 @@
+Template.activities.created = function () {
+  var showLatestActivities = new ReactiveVar();
+};
+
 Template.activities.rendered = function () {
   var elem = document.querySelector("[name='all-or-latest']");
   var init = new Switchery(elem);
