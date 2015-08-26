afbd88afbe142d3a33201071b3218c2fcced1596
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-26 20:05:41 +0300

    Add getAllActivityTypeIds

---
+++ server/methods/activityTypes.js
@@ -0,0 +1,15 @@
+Meteor.methods({
+  'getAllActivityTypeIds': function () {
+    // TODO: determine how to secure this method to prevent client abuse
+
+    // Get all Activity Types
+    var activityTypes = ActivityTypes.find().fetch();
+
+    // Create an array of Activity Type IDs
+    var activityTypeIds = _.map(activityTypes, function (activityType) {
+      return activityType._id;
+    });
+
+    return ActivityTypeIds;
+  },
+});
