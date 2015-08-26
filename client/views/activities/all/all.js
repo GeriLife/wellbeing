1864a08d25687a100468eca688a8337a4f27abca
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-26 19:31:47 +0300

    Add activities and tableSettings helpers

--- client/views/activities/all/all.js
+++ client/views/activities/all/all.js
@@ -5,3 +5,36 @@ Template.allActivities.created = function () {
   // Subscribe to all activities and related collections
   instance.subscribe("activitiesComposite");
 };
+
+Template.allActivities.helpers({
+  "activities": function () {
+    // Query Activities collection for all activities
+    var activities = Activities.find().fetch();
+
+    // Create a placeholder array for resident objects
+    var activitiesArray = [];
+
+    // Iterate through activities
+    activities.forEach(function (activity) {
+      // set resident names, type, duration, and date values
+      var activityObject = {
+        residents: activity.residentNames(),
+        type: activity.activityType(),
+        duration: activity.duration,
+        date: activity.activityDate
+      };
+
+      // Add activity object to residents list
+      activitiesArray.push(activityObject);
+    });
+
+    return activitiesArray;
+  },
+  'tableSettings': function () {
+    var tableSettings = {
+      showFilter: false
+    };
+
+    return tableSettings;
+  }
+});
