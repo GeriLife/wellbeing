599e7752a92804db65bbdcf6ba191e183112f9e5
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-27 09:52:50 +0300

    Create reactive variable and template helper

--- client/views/activities/latest/latestByType.js
+++ client/views/activities/latest/latestByType.js
@@ -4,4 +4,28 @@ Template.latestActivitiesByType.created = function () {
 
   // Create variable to hold activity type selection
   instance.activityTypeSelection = new ReactiveVar();
+
+  // Create a reactive var to hold data containing latest activities by type
+  instance.latestActivitiesByTypeData = new ReactiveVar();
+
+  Meteor.call('getAllResidentsLatestActivitiesByType', function (error, latestResidentActivitiesByType) {
+    // Set latest activities by type
+    instance.latestActivitiesByTypeData.set(latestResidentActivitiesByType);
+  });
 };
+
+Template.latestActivitiesByType.helpers({
+  'latestActivitiesByType': function () {
+    // Get a reference to template instance
+    var instance = Template.instance();
+
+    var latestResidentActivitiesByType = instance.latestActivitiesByTypeData.get();
+
+    //console.log(latestResidentActivitiesByType);
+    if (latestResidentActivitiesByType) {
+      return latestResidentActivitiesByType;
+    } else {
+      return [];
+    }
+  }
+});
