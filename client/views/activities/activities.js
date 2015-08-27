750c18c090e073c78942aa5a1d203cbd449d5b6a
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-28 01:53:26 +0300

    Add showLatestActivities helper; cleanup

--- client/views/activities/activities.js
+++ client/views/activities/activities.js
@@ -3,25 +3,6 @@ Template.activities.created = function () {
   var instance = this;
 
   instance.showLatestActivities = new ReactiveVar();
-
-  // Instance subscriptions
-  // Activity types, residents, and homes
-  instance.subscribe('allActivityTypes');
-  instance.subscribe('allResidents');
-  instance.subscribe('allHomes');
-
-  instance.autorun(function () {
-    // Determine whether to show only latest activities
-    if (instance.showLatestActivities.get() === true) {
-      // If show latest activities is true
-      // subscribe to latest activities and related collections
-      instance.subscribe("residentLatestActivities");
-    } else if (instance.showLatestActivities.get() === false) {
-      // If show latest activities is false
-      // subscribe to all activities and related collections
-      instance.subscribe("allActivities");
-    }
-  });
 };
 
 Template.activities.rendered = function () {
@@ -52,84 +33,11 @@ Template.activities.events({
 
 
 Template.activities.helpers({
-  "activities": function () {
-    // Query Activities collection for all activities
-    var activities = Activities.find().fetch();
+  'showLatestActivities': function () {
+    // Get reference to template instance
+    var instance = Template.instance();
 
-    // Create a placeholder array for resident objects
-    var activitiesArray = [];
-
-    // Iterate through activities
-    activities.forEach(function (activity) {
-      // Get all resident IDs
-      var residentIds = activity.residentIds;
-
-      // Placeholder for resident names
-      var homeNames = _.map(residentIds, function (residentId) {
-        // Get the resident
-        var resident = Residents.findOne(residentId);
-
-        // Get resident home name
-        var homeName = resident.homeName();
-
-        return homeName;
-      });
-
-      // Get only unique home names
-      var homeNamesUnique = _.uniq(homeNames);
-
-      // set resident names, type, duration, and date values
-      var activityObject = {
-        residents: activity.residentNames(),
-        type: activity.activityType(),
-        duration: activity.duration,
-        activityDate: activity.activityDate,
-        homeNames: homeNamesUnique
-      };
-
-      // Add activity object to residents list
-      activitiesArray.push(activityObject);
-    });
-
-    return activitiesArray;
-  },
-  'tableSettings': function () {
-    var tableSettings = {
-      showFilter: false,
-      filters: ['residentFilter', 'homeFilter', 'typeFilter'],
-      fields: [
-        {
-          key: 'residents',
-          label: 'Resident(s)',
-          sortOrder: 1,
-          sortDirection: 'ascending'
-        },
-        {
-          key: 'homeNames',
-          label: 'Resident(s) Home(s)',
-          sortOrder: 2,
-          sortDirection: 'ascending'
-        },
-        {
-          key: 'type',
-          label: 'Activity Type',
-          sortOrder: 2,
-          sortDirection: 'ascending'
-        },
-        {
-          key: 'duration',
-          label: 'Duration',
-        },
-        {
-          key: 'activityDate',
-          label: 'Activity Date',
-          sortOrder: 0,
-          sortDirection: 'descending',
-          tmpl: Template.dateCell,
-        }
-      ],
-    };
-
-    return tableSettings;
+    // Return value of show latest activities toggle
+    return instance.showLatestActivities.get();
   }
 });
