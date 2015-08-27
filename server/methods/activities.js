a8e534a2f9e614bd695f60f485f31d4cbf60ad52
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-27 09:53:42 +0300

    Add getAllResidentsLatestActivitiesByType method

--- server/methods/activities.js
+++ server/methods/activities.js
@@ -1,5 +1,5 @@
 Meteor.methods({
-  'getAllResidentsLatestActivitiesByType': function () {
+   'getAllResidentsLatestActivitiesByType': function () {
     /*
     Return an array of Activity IDs
     where each activity type has at most one entry per resident
@@ -11,16 +11,35 @@ Meteor.methods({
     // Get all Activity Type IDs
     var activityTypeIds = Meteor.call('getAllActivityTypeIds');
 
-    // Iterate through resident IDs
-    // for each resident ID
-      // Iterate through activity types
-      // foe each activity type,
-          // get resident latest activity
-          // append activity ID to residentsLatestActivityIdsByType
-
     // Placeholder for residents latest activity
-    var residentsLatestActivityIdsByType = [];
+    var residentsLatestActivitiesByType = [];
 
-    // return residentsLatestActivityIdsByType
+    // Iterate through resident IDs
+    residentIds.forEach(function (residentId) {
+      // for each resident ID
+      // Iterate through activity types
+      console.log(residentId);
+      activityTypeIds.forEach(function (activityTypeId) {
+        // Get a reference to Resident ID (as array-like object apparently)
+        var residentIdObject = this;
+
+        // Convert the resident ID array-like object to an array
+        var residentIdArray = Array.prototype.slice.call(residentIdObject);
+
+        // Convert the resident ID array to a string
+        var residentId = residentIdArray.join("");
+
+        // TODO: see if there is a simple way to accept a callback argument as string
+
+        // for each activity type,
+        // get resident latest activity
+        var residentLatestActivityByType = Meteor.call('getResidentLatestActivityByType', residentId, activityTypeId);
+
+        // append activity ID to residentsLatestActivitiesByType
+        residentsLatestActivitiesByType.push(residentLatestActivityByType);
+      }, residentId);
+    });
+    console.log(residentsLatestActivitiesByType);
+    return residentsLatestActivitiesByType
   }
 });
