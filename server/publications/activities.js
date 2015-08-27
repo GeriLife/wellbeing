100fa104f3fff7e06f6e83717d47b40da51d02ea
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-28 01:33:41 +0300

    Add residentLatestActivities publication

--- server/publications/activities.js
+++ server/publications/activities.js
@@ -11,3 +11,11 @@ Meteor.publish('residentActivities', function (residentId) {
   // Activities for a given user
   return Activities.find({'residentIds':  residentId });
 });
+
+Meteor.publish('residentLatestActivities', function () {
+  // Get latest activity IDs
+  var latestActivityIds = Meteor.call('getAllResidentsLatestActivityIdsByType');
+
+  // Get cursor to latest activities
+  return Activities.find({_id: {$in: latestActivityIds}});
+});
