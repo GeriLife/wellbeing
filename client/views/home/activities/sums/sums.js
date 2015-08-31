de9a8accb6e2e6de7b61227086e5d3a44036beea
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-31 19:20:55 +0300

    Instance created and rendered functions

--- client/views/home/activities/sums/sums.js
+++ client/views/home/activities/sums/sums.js
@@ -2,13 +2,40 @@ Template.homeResidentActivitySumsByType.created = function () {
   // Get reference to template instance
   var instance = this;
 
+  // Get current home ID
+  instance.homeId = Router.current().params.homeId;
+
   // set up home resident activity sums by type reactive variable
-  var homeResidentsActivitySumsByType = new ReactiveVar();
+ instance.homeResidentsActivitySumsByType = new ReactiveVar();
 
   // Get home resident activity sums from server method
-  Meteor.call('getHomeResidentsActivitySumsByType', function (error, activitySums) {
+  Meteor.call('getHomeResidentsActivitySumsByType', instance.homeId, function (error, activitySums) {
     //Set the home resident activity sums variable with the returned activity sums
-    homeResidentsActivitySumsByType.set(activitySums);
+    instance.homeResidentsActivitySumsByType.set(activitySums);
+  });
+
+  instance.autorun(function () {
+    var homeResidentsActivitySumsByType = instance.homeResidentsActivitySumsByType.get();
+
+   if (instance.view.isRendered && homeResidentsActivitySumsByType) {
+      // Draw the chart
+      var activityChart = new dimple.chart(instance.chartSvg, homeResidentsActivitySumsByType);
+      activityChart.setBounds(60, 45, 510, 315)
+      var residentAxis = activityChart.addCategoryAxis("x", "residentName");
+     residentAxis.title = "Resident Name / Activity Count";
+      var activitiesAxis = activityChart.addMeasureAxis("y", "sum");
+     activitiesAxis.title = "Number of Activities";
+      activityChart.addSeries("activityTypeName", dimple.plot.bar);
+      activityChart.addLegend(200, 10, 380, 20, "right");
+      activityChart.draw();
+    };
   });
 };
 
+Template.homeResidentActivitySumsByType.rendered = function () {
+  // Get reference to template instance
+  var instance = this;
+
+  // Get reference to chart div
+ instance.chartSvg = dimple.newSvg("#residentActivitiesSummary", 600, 400);
+};
