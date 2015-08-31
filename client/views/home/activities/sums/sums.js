9b2a3f582ff13a23a7e35e1f5a2968cfe046d10f
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-31 18:17:08 +0300

    Call getHomeResidentsActivitySumsByType

---
+++ client/views/home/activities/sums/sums.js
@@ -0,0 +1,11 @@
+Template.homeResidentActivitySumsByType.created = function () {
+  // set up home resident activity sums by type reactive variable
+  var homeResidentsActivitySumsByType = new ReactiveVar();
+
+  // Get home resident activity sums from server method
+  Meteor.call('getHomeResidentsActivitySumsByType', function (error, activitySums) {
+    //Set the home resident activity sums variable with the returned activity sums
+    homeResidentsActivitySumsByType.set(activitySums);
+  });
+};
+
