27a4336da60e1f722bb12ac4a08aa1698ce47d98
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-09-04 00:19:20 +0300

    Add homes route

--- client/routes/homes.js
+++ client/routes/homes.js
@@ -1,6 +1,11 @@
 /*
   Homes routes
 */
+Router.route('/homes', function () {
+  this.render('homes');
+}, {
+  name: 'homes'
+});
 
 Router.route('/home/:homeId', function () {
   var controller = this;
