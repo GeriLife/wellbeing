b1a5c1870aabeaf52bbba5d4867ee3f4828ce534
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-11-28 08:24:33 +0200

    Add brylie:first-user-admin

--- both/adminConfig.js
+++ both/adminConfig.js
@@ -4,38 +4,10 @@ AdminConfig = {
   skin: "blue-light",
   collections:
   {
-    Residents: {
-      icon: "users",
-      color: "red",
-      auxCollections: ['Homes'],
-      tableColumns: [
-        {label: "First name", name: "firstName"},
-        {label: "Last Initial", name: "lastInitial"}
-      ],
-      extraFields: ['homeId']
-    },
     ActivityTypes: {
       icon: "heart-o",
       label: "Activity Types",
       color: "green",
-      tableColumns: [
-        {label: "Name", name: "name"}
-      ]
-    },
-    Homes: {
-      icon: "home",
-      color: "yellow",
-      auxCollections: ['Groups'],
-      tableColumns: [
-        {label: "Name", name: "name"},
-        {label: "Group", name: "groupName()"}
-      ],
-      extraFields: ['groupId']
-    },
-    Groups: {
-      icon: "sitemap",
-      label: "Home Groups",
-      color: "yellow",
       tableColumns: [
         {label: "Name", name: "name"}
       ]
