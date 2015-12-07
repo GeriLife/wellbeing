18177c0ca48f854b96d59d5c17f24f175ca629d7
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-12-07 19:47:31 +0200

    Rename

---
+++ client/views/activityTypes/activityTypesList.js
@@ -0,0 +1,15 @@
+Template.activityTypesList.created = function () {
+  // Get reference to template instance
+  var instance = this;
+
+  instance.subscribe('allActivityTypes');
+};
+
+Template.activityTypesList.helpers({
+  "activityTypes": function () {
+    // Get all activity types
+    var activityTypes = ActivityTypes.find({}, {sort: {name: 1}}).fetch();
+
+    return activityTypes;
+  }
+});
