dc021a55e91c21f709ffa752e66a237ed70ff541
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-12-17 02:26:44 +0200

    Add getResidentWeeklyActivitiesCountFromDate

--- server/methods/residentActivities.js
+++ server/methods/residentActivities.js
@@ -13,5 +13,21 @@ Meteor.methods({
     }).count();
 
     return activityCount;
+  },
+  'getResidentWeeklyActivitiesCountFromDate': function (residentId, date) {
+    // Date to start activity range query
+    var start = moment(date).subtract(1, "weeks").toDate();
+    //console.log(start);
+    // Date to end activity range query
+    var end = new moment(date).toDate();
+    //console.log(end);
+
+    // Get a count of all resident activities within the desired time range
+    var activityCount = Activities.find({
+      'residentIds': residentId,
+      activityDate: {$gte: start, $lte: end}
+    }).count();
+
+    return activityCount;
   }
 });
