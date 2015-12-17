03a5b761bac24c41902a8f7ebfdda561122ae2bf
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-12-17 02:26:56 +0200

    Add getHomeActivityCountTrend, work-in-progress

--- server/methods/homes.js
+++ server/methods/homes.js
@@ -93,5 +93,44 @@ Meteor.methods({
     }
 
     return residentActivityLevelCounts;
+  },
+  "getHomeActivityCountTrend": function (homeId) {
+    // Get home residents
+    var residentIds = Meteor.call("getHomeResidentIds",homeId);
+
+
+    var numberOfDays = 7;
+    var activityCountsArray = [];
+
+    for (var i = 0; i < numberOfDays; i++) {
+      // Get today's date
+      var day = moment().subtract(i, "days");
+
+      // Set up placeholder activity counts object
+      var dailyActivityCounts = {
+        date: day.toDate(),
+        inactive: 0,
+        semiActive: 0,
+        active: 0
+      };
+
+      // Get activity level for each resident
+      // Compare it against the baseline
+      _.each(residentIds, function (residentId) {
+        var result = Meteor.call("getResidentWeeklyActivitiesCountFromDate", residentId, day);
+        if (result === 0) {
+          dailyActivityCounts.inactive =+ 1;
+        } else if (result < 5) {
+          dailyActivityCounts.semiActive += 1;
+        } else if (result >= 5) {
+          dailyActivityCounts.active += 1;
+        };
+      });
+
+      // Add daily activity levels to activity counts array
+      activityCountsArray.push(dailyActivityCounts);
+    };
+
+    console.log(activityCountsArray);
   }
 });
