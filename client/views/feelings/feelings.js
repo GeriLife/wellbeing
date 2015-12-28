7253d91e3e6b93f07ff07512208c9e569e9dd93b
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-12-28 16:05:48 +0200

    Cleanup

--- client/views/feelings/feelings.js
+++ client/views/feelings/feelings.js
@@ -2,26 +2,21 @@ Template.feelings.events({
   "click #joy": function () {
     // Set selected feeling session variable
     Session.set("selectedFeeling", "joy");
-    console.log("joy clicked");
   },
   "click #fear": function () {
     // Set selected feeling session variable
     Session.set("selectedFeeling", "fear");
-    console.log("fear clicked");
   },
   "click #sad": function () {
     // Set selected feeling session variable
     Session.set("selectedFeeling", "sad");
-    console.log("sad clicked");
   },
   "click #anger": function () {
     // Set selected feeling session variable
     Session.set("selectedFeeling", "anger");
-    console.log("anger clicked");
   },
   "click .feelings": function () {
     // Show feelings modal
-    console.log("feelings clicked");
     Modal.show('newFeeling');
   }
 });
