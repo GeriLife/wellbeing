6aa95dfd278a7fee3dfbf9ddedcd5c3770d33691
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-12-28 15:10:42 +0200

    Move AutoForm hook

--- client/views/activity/new/newActivity.js
+++ client/views/activity/new/newActivity.js
@@ -1,10 +1,3 @@
-AutoForm.addHooks('new-activity-form', {
-    onSuccess: function(operation, resultId, template) {
-        // Alert user that activity was added
-        FlashMessages.sendSuccess('<i class="fa fa-check"></i> Activity added!');
-    }
-});
-
 Template.newActivity.created = function () {
   this.subscribe('allResidents');
   this.subscribe('allHomes');
