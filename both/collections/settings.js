4915fc1f7779c69367163b213d5faccb1c1bf2d7
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-01-02 15:10:25 +0200

    Fix update allow rule

--- both/collections/settings.js
+++ both/collections/settings.js
@@ -9,12 +9,15 @@ Settings.allow({
     if (settingsCount === 0) {
       // Get ID of current Meteor user
       var userId = Meteor.userId();
-      
+
       // Only allow admin users to insert settings
       return Roles.userIsInRole(userId, ['admin']);
     }
   },
   update: function () {
+    // Get ID of current Meteor user
+    var userId = Meteor.userId();
+    
     // Only allow admin users to update settings
     return Roles.userIsInRole(userId, ['admin']);
   }
