76752bb1b91e59de496ea6d2aebe5f2854641e5d
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-01-05 14:47:29 +0200

    Updated permissions

--- both/collections/settings.js
+++ both/collections/settings.js
@@ -2,22 +2,23 @@ Settings = new Mongo.Collection("settings");
 
 Settings.allow({
   insert: function () {
-    // Get a count of settings documents
-    var settingsCount = Settings.find().count();
+    // Get ID of current Meteor user
+    var userId = Meteor.userId();
 
-    // Make sure only one settings document exists
-    if (settingsCount === 0) {
-      // Get ID of current Meteor user
-      var userId = Meteor.userId();
-
-      // Only allow admin users to insert settings
-      return Roles.userIsInRole(userId, ['admin']);
-    }
+    // Only allow admin users to insert settings
+    return Roles.userIsInRole(userId, ['admin']);
   },
   update: function () {
     // Get ID of current Meteor user
     var userId = Meteor.userId();
-    
+
+    // Only allow admin users to update settings
+    return Roles.userIsInRole(userId, ['admin']);
+  },
+  remove: function () {
+    // Get ID of current Meteor user
+    var userId = Meteor.userId();
+
     // Only allow admin users to update settings
     return Roles.userIsInRole(userId, ['admin']);
   }
