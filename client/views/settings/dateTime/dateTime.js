0fa09dcf7e3c17c368f1e49bb6923ef2cd445fe7
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-01-05 15:09:19 +0200

    Set select field to current timezone setting

--- client/views/settings/dateTime/dateTime.js
+++ client/views/settings/dateTime/dateTime.js
@@ -7,8 +7,23 @@ Template.dateTimeSettings.created = function () {
 };
 
 Template.dateTimeSettings.rendered = function () {
+  // Get reference to template instance
+  var instance = this;
+
   // Add timezones to the timezone select box
   $('#timezone-select').timezones();
+
+  // Populate timezone select with current setting, if available
+  instance.autorun(function () {
+    // Get current timezone setting
+    var timezoneSetting = Settings.find({name: "timezone"}).fetch()[0];
+
+    // Check for current setting
+    if (timezoneSetting) {
+      // Set the value of the timezone select
+      $('#timezone-select').val(timezoneSetting.value);
+    }
+  });
 };
 
 Template.dateTimeSettings.events({
