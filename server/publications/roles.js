641eecd79b5180d6e02980b077a6a923af805ff0
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-01-06 19:55:48 +0200

    Add allRoles publication

--- server/publications/roles.js
+++ server/publications/roles.js
@@ -1,3 +1,8 @@
+Meteor.publish("allRoles", function () {
+  // Return all roles
+  return Meteor.roles.find();
+});
+
 Meteor.publish("allRolesExceptAdmin", function () {
   // Get all roles except 'admin'
   var roles = Meteor.roles.find({name: {$ne: "admin"}});
