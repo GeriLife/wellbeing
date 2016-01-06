f65bc4141386d688c94634ab80d077ae726b9549
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-01-06 18:29:26 +0200

    Add emails helper

--- client/views/settings/users/users.js
+++ client/views/settings/users/users.js
@@ -5,3 +5,17 @@ Template.usersSettings.created = function () {
   // Subscribe to all users
   instance.subscribe("allUsers");
 };
+
+Template.usersSettings.helpers({
+  "users": function () {
+    // Get all users
+    var users = Meteor.users.find().fetch();
+    console.log(users);
+    return users;
+  },
+  email: function () {
+		if (this.emails && this.emails.length) {
+      return this.emails[0].address;
+    }
+  }    
+});
