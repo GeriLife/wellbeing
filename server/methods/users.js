459050e66443097fc9c95e2bd5672435efb2fa07
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-01-09 11:55:50 +0200

    Add editUserFormSubmit

--- server/methods/users.js
+++ server/methods/users.js
@@ -5,6 +5,17 @@ Meteor.methods({
 
     return userId;
   },
+  "editUserFormSubmit": function (user) {
+    // Get user email
+    var userEmail = user.email;
+
+    var userId = user._id;
+
+    // Edit user, setting first email
+    Meteor.users.update(userId, {$set: {"emails.0.address": userEmail}});
+
+    return userId;
+  },
   "addUserToAdminRole": function (userId) {
     // Add user to admin role
     Roles.addUsersToRoles(userId, "admin");
