92742303129223e8f090053557329dff11e92738
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-01-13 09:47:51 +0200

    Fix insert, update, remove allow rules

--- both/collections/residents.js
+++ both/collections/residents.js
@@ -115,15 +115,63 @@ Residents.helpers({
 
 Residents.allow({
   'insert': function () {
-    // Only allow administrators
-    return Roles.userIsInRole(userId, ['admin']);
+    // Get user ID
+    let userId = Meteor.userId();
+
+    // Placeholder for administrator check
+    let userIsAdministrator;
+
+    // Placeholder for insert privilege check
+    let userCanInsert;
+
+    if (userId) {
+      // Check if user is administrator
+      userIsAdministrator = Roles.userIsInRole(userId, ['admin']);
+    }
+
+    // Only allow adminstator users insert
+    userCanInsert = (userId && userIsAdministrator);
+
+    return userCanInsert;
   },
   'update': function () {
-    // Only allow administrators
-    return Roles.userIsInRole(userId, ['admin']);
+    // Get user ID
+    let userId = Meteor.userId();
+
+    // Placeholder for administrator check
+    let userIsAdministrator;
+
+    // Placeholder for update privilege check
+    let userCanUpdate;
+
+    if (userId) {
+      // Check if user is administrator
+      userIsAdministrator = Roles.userIsInRole(userId, ['admin']);
+    }
+
+    // Only allow adminstator users insert
+    userCanUpdate = (userId && userIsAdministrator);
+
+    return userCanUpdate;
   },
   'remove': function () {
-    // Only allow administrators
-    return Roles.userIsInRole(userId, ['admin']);
+    /// Get user ID
+    let userId = Meteor.userId();
+
+    // Placeholder for administrator check
+    let userIsAdministrator;
+
+    // Placeholder for insert privilege check
+    let userCanRemove;
+
+    if (userId) {
+      // Check if user is administrator
+      userIsAdministrator = Roles.userIsInRole(userId, ['admin']);
+    }
+
+    // Only allow adminstator users insert
+    userCanRemove = (userId && userIsAdministrator);
+
+    return userCanRemove;
   }
 });
