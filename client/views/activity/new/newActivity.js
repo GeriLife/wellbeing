260cce89e50ccc6a910006ff9189ec4dc92539e2
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-01-13 19:51:36 +0200

    Subscribe to allRolesExceptAdmin

--- client/views/activity/new/newActivity.js
+++ client/views/activity/new/newActivity.js
@@ -2,4 +2,5 @@ Template.newActivity.created = function () {
   this.subscribe('allCurrentResidents');
   this.subscribe('allHomes');
   this.subscribe('allActivityTypes');
+  this.subscribe('allRolesExceptAdmin');
 };
