ff03b1c0e1bf32dd485e95853b79adb26239260e
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-07 18:39:22 +0200

    Add residentActivityLevel tags

--- client/views/home/residents/resident/resident.js
+++ client/views/home/residents/resident/resident.js
@@ -44,6 +44,11 @@ Template.homeResident.helpers({
     // Get resident activity count
     var activityCount = instance.residentActivityCount.get();
 
+    // Get activity level translations
+    const active = TAPi18n.__("residentActivityLevel-active");
+    const semiActive = TAPi18n.__("residentActivityLevel-semiActive");
+    const inactive = TAPi18n.__("residentActivityLevel-inactive");
+
     // Case for returning Bootstrap class based on activity level
     if (activityCount >= 5) {
       return '(Active)';
