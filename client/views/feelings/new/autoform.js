0038813c3409cbcff223c411a91649bed3c274db
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-07 16:56:21 +0200

    Get translated success message

--- client/views/feelings/new/autoform.js
+++ client/views/feelings/new/autoform.js
@@ -3,7 +3,10 @@ AutoForm.addHooks(['newFeelingForm'], {
     // Hide the modal dialogue
     Modal.hide('newFeeling');
 
+    // Get success message translation
+    const successMessage = TAPi18n.__("newFeeling-success");
+
     // Alert user that feeling was recorded
-    FlashMessages.sendSuccess('<i class="fa fa-check"></i> Feeling recorded.');
+    FlashMessages.sendSuccess('<i class="fa fa-check"></i> ' + successMessage);
   }
 });
