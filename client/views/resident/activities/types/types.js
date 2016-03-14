14964b60d5d136925abbbc78208976abf9bdc6d5
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-14 18:03:18 +0200

    Get activity type counts

--- client/views/resident/activities/types/types.js
+++ client/views/resident/activities/types/types.js
@@ -1,3 +1,16 @@
 Template.residentActivityTypesChart.onRendered(function () {
-  console.log(Template.currentData());
+  let activities = Template.currentData().activities;
+
+  activities = _.map(activities, function (activity) {
+    activity.type = activity.activityType();
+    return activity;
+  });
+
+  // Group activities by activity type
+  const activityTypeCounts = d3.nest()
+    .key(function (activity) {
+      return activity.type;
+    })
+    .rollup(function (activityType) { return activityType.length })
+    .entries(activities);
 });
