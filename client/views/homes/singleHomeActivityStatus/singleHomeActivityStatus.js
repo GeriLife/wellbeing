3868565c26a23b5498a87a2220353293748c03ee
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-19 11:42:14 +0200

    Set up activityLevelCounts instance reactive var

--- client/views/homes/singleHomeActivityStatus/singleHomeActivityStatus.js
+++ client/views/homes/singleHomeActivityStatus/singleHomeActivityStatus.js
@@ -1,10 +1,17 @@
-Template.singleHomeActivityStatus.helpers({
-  "activityLevelCounts": function () {
+Template.singleHomeActivityStatus.onCreated(function () {
     var instance = this;
 
     // Get ID of current home
-    var homeId = this._id;
+    var homeId = instance.data.home._id;
 
-    return ReactiveMethod.call("getHomeActivityLevelCounts", homeId);
-  }
-});
+    // Add variable to hold activity counts
+    instance.activityLevelCounts = new ReactiveVar();
+
+    instance.autorun(function () {
+      // Retrieve home resident activity level counts from server
+      const activityLevelCounts = ReactiveMethod.call("getHomeActivityLevelCounts", homeId);
+
+      // Update the reactive variable, to trigger the graph to render
+      instance.activityLevelCounts.set(activityLevelCounts);
+    });
+  });
