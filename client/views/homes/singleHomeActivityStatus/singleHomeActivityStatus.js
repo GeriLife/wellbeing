b95c2da7e99f7b5ad81b245ac6eee74fd4dbc7ba
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-19 12:40:26 +0200

    Restructure activityLevelCounts for chart library

--- client/views/homes/singleHomeActivityStatus/singleHomeActivityStatus.js
+++ client/views/homes/singleHomeActivityStatus/singleHomeActivityStatus.js
@@ -2,16 +2,35 @@ Template.singleHomeActivityStatus.onCreated(function () {
     var instance = this;
 
     // Get ID of current home
-    var homeId = instance.data.home._id;
+    instance.homeId = instance.data.home._id;
 
     // Add variable to hold activity counts
     instance.activityLevelCounts = new ReactiveVar();
 
     instance.autorun(function () {
       // Retrieve home resident activity level counts from server
-      const activityLevelCounts = ReactiveMethod.call("getHomeActivityLevelCounts", homeId);
+      const activityLevelCounts = ReactiveMethod.call("getHomeActivityLevelCounts", instance.homeId);
 
-      // Update the reactive variable, to trigger the graph to render
-      instance.activityLevelCounts.set(activityLevelCounts);
+      // Make sure activity level counts exist
+      if (activityLevelCounts) {
+        /*
+        Re-structure activity level counts data to an object containing
+        level and count attributes
+        */
+        const activityLevelTypes = _.keys(activityLevelCounts);
+
+        const activityLevelData = _.map(activityLevelTypes, function (type) {
+          // Construct an object with the type and count keys
+          const activityLevelCount = {
+            type: type,
+            count: activityLevelCounts[type]
+          };
+
+          return activityLevelCount;
+        });
+
+        // Update the reactive variable, to trigger the graph to render
+        instance.activityLevelCounts.set(activityLevelCounts);
+      }
     });
   });
