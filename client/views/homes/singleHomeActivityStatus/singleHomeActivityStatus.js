a87945d9e156e409749c8d276ac7be7831d5eac8
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-20 04:21:58 +0200

    Adjust chart boundaries

--- client/views/homes/singleHomeActivityStatus/singleHomeActivityStatus.js
+++ client/views/homes/singleHomeActivityStatus/singleHomeActivityStatus.js
@@ -68,7 +68,7 @@ Template.singleHomeActivityStatus.onRendered(function () {
   const activityLevelsChart = new dimple.chart(svg, []);
 
   // Set chart boundaries
-  activityLevelsChart.setBounds("15%", "0%", "80%", "65%");
+  activityLevelsChart.setBounds(10, 0, "90%", "65%");
 
   // Add home resident percentage to x axis
   const xAxis = activityLevelsChart.addMeasureAxis("x", "homePercentage");
