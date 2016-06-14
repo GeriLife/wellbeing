6547e0b3baec719d4f232bec2b61c29ce0882c12
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-06-14 20:29:28 +0300

    Add select2Options

--- client/views/activity/new/newActivity.js
+++ client/views/activity/new/newActivity.js
@@ -4,3 +4,11 @@ Template.newActivity.created = function () {
   this.subscribe('allActivityTypes');
   this.subscribe('allRolesExceptAdmin');
 };
+
+Template.newActivity.helpers({
+  select2Options () {
+    return {
+      multiple: true
+    }
+  }
+});
