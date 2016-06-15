d32bd4eee4a169921ed091829849cd789f274be1
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-06-15 21:14:02 +0300

    Remove select2Options; render Chosen when AutoForm renders

--- client/views/activity/new/newActivity.js
+++ client/views/activity/new/newActivity.js
@@ -5,10 +5,25 @@ Template.newActivity.created = function () {
   this.subscribe('allRolesExceptAdmin');
 };
 
-Template.newActivity.helpers({
-  select2Options () {
-    return {
-      multiple: true
-    }
+
+Template.autoForm.onRendered(function () {
+  const instance = this;
+
+  // Make sure the new activity form is being rendered
+  if (instance.data.id === "newActivityForm") {
+    // Get the user agent type
+    var deviceAgent = navigator.userAgent.toLowerCase();
+
+    // Check if the device is iOS
+    var iOS = deviceAgent.match(/(iPad|iPhone|iPod)/i);
+
+    // If the device is not iOS, render the chosen select widget
+    if (!iOS) {
+      // Get a reference to the resident select field
+      const residentSelect = $('[name=residentIds]');
+
+      // Activate the improved multi-select widget
+      residentSelect.chosen();
+    };
   }
 });
