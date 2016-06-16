0d3790074a476ea2d9593b785f9ea2ac61028bb9
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-06-17 01:41:23 +0300

    Enable Bootstrap Select when AutoForm loads

--- client/views/activity/new/newActivity.js
+++ client/views/activity/new/newActivity.js
@@ -11,19 +11,12 @@ Template.autoForm.onRendered(function () {
 
   // Make sure the new activity form is being rendered
   if (instance.data.id === "newActivityForm") {
-    // Get the user agent type
-    var deviceAgent = navigator.userAgent.toLowerCase();
+    // Get a reference to the resident select field
+    const residentSelect = $('[name=residentIds]');
 
-    // Check if the device is iOS
-    var iOS = deviceAgent.match(/(iPad|iPhone|iPod)/i);
-
-    // If the device is not iOS, render the chosen select widget
-    if (!iOS) {
-      // Get a reference to the resident select field
-      const residentSelect = $('[name=residentIds]');
-
-      // Activate the improved multi-select widget
-      residentSelect.chosen();
-    };
+    // Activate the improved multi-select widget
+    residentSelect.selectpicker({
+      header: "Select resident(s)"
+    });
   }
 });
