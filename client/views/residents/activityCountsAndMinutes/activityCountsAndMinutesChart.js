941d55c678291ec8e63945a0a3089dda48c0a99e
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-07-04 21:57:19 +0300

    Initial chart logic on render

--- client/views/residents/activityCountsAndMinutes/activityCountsAndMinutesChart.js
+++ client/views/residents/activityCountsAndMinutes/activityCountsAndMinutesChart.js
@@ -9,3 +9,21 @@ Template.residentsRecentActivityCountsAndMinutesChart.onCreated(function () {
     instance.residentAcitivityCountsAndMinutes.set(residentActivityCountsAndMinutes);
   });
 });
+
+Template.residentsRecentActivityCountsAndMinutesChart.onRendered(function () {
+  // Get reference to template instance
+  var instance = this;
+
+  // Set up resident activity counts and minutes chart
+  var residentActivityCountsAndMinutesChart = nv.models.scatterChart();
+
+  // Set up chart styles
+  residentActivityCountsAndMinutesChart
+    .color(d3.scale.category10().range())
+    .duration(300)
+
+  // Set up axis tickmarks
+  residentActivityCountsAndMinutesChart
+    .xAxis.tickFormat(d3.format('d'))
+    .yAxis.tickFormat(d3.format('d'));
+});
