7c2925d9ebdda89d926d8b20437aa16bb1ee5249
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-07-04 21:53:46 +0300

    Instance reactive variable for chart data

--- client/views/residents/activityCountsAndMinutes/activityCountsAndMinutesChart.js
+++ client/views/residents/activityCountsAndMinutes/activityCountsAndMinutesChart.js
@@ -1,4 +1,11 @@
 Template.residentsRecentActivityCountsAndMinutesChart.onCreated(function () {
   // Get reference to template instance
   var instance = this;
+
+  // Create reactive variable to hold resident recent activitiy count and minutes
+  instance.residentAcitivityCountsAndMinutes = new ReactiveVar();
+
+  Meteor.call("getResidentsRecentActivityMinutesAndCountsByHome", function (error, residentActivityCountsAndMinutes) {
+    instance.residentAcitivityCountsAndMinutes.set(residentActivityCountsAndMinutes);
+  });
 });
