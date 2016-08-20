ce3e8da41c1846c2a662b30d712ee16926f634f8
Brylie Christopher Oxley <brylie@amble.fi>
2016-08-20 17:26:55 +0300

    Refresh chart when data changes

--- client/views/resident/activities/chart/chart.js
+++ client/views/resident/activities/chart/chart.js
@@ -5,62 +5,66 @@ Template.residentActivityTrendChart.rendered = function () {
   // Get resident name
   const residentName = instance.data.firstName + " " + instance.data.lastInitial;
 
-  // Get the resident activities from template instance
-  const activities = instance.data.activities;
+  instance.autorun(function () {
+    $("#activity-chart").empty();
+    
+    // Get the resident activities from template instance
+    const activities = Template.currentData().activities;
 
-  // Group activities by activity date
-  const nestedActivities = d3.nest()
-    .key(function (activity) {
-      return activity.activityDate;
-    })
+    // Group activities by activity date
+    const nestedActivities = d3.nest()
+      .key(function (activity) {
+        return activity.activityDate;
+      })
 
-  // Get a sum of activities
-  const summedActivities = nestedActivities.rollup(function (activity) {
-      return {
-        duration: d3.sum(activity, function (activity) {
-          return activity.duration;
-        })
+    // Get a sum of activities
+    const summedActivities = nestedActivities.rollup(function (activity) {
+        return {
+          duration: d3.sum(activity, function (activity) {
+            return activity.duration;
+          })
+        }
+      })
+      .entries(activities);
+
+      summedActivities.map(function (activity) {
+        // Create date and duration attributes with proper data types
+        activity.date = new Date(activity.key);
+        activity.duration = parseInt(activity.values.duration);
+
+        // Delete unused key and values
+        delete activity.values
+        delete activity.key;
+      })
+
+    // Get translation strings
+    const activityTrendTitle = TAPi18n.__("residentActivityTrendChart-title");
+    const activityTrendDescription = TAPi18n.__("residentActivityTrendChart-description");
+    const yLabel = TAPi18n.__("residentActivityTrendChart-yLabel");
+    const mouseoverLabel = TAPi18n.__("residentActivityTrendChart-mouseoverLabel");
+
+    // Render the chart using MetricsGraphics charting library
+    MG.data_graphic({
+      title: activityTrendTitle,
+      description: activityTrendDescription,
+      y_label: yLabel,
+      y_rug: true,
+      data: summedActivities,
+      chart_type: 'point',
+      least_squares: true,
+      full_width: true,
+      height: 200,
+      right: 40,
+      target: document.getElementById('activity-chart'),
+      x_accessor: 'date',
+      y_accessor: 'duration',
+      mouseover: function (d, i) {
+        const date = d.point.date;
+        const formattedDate = moment(date).format("D.M.YYYY");
+        const minutes = d.point.duration;
+        d3.select('#activity-chart svg .mg-active-datapoint')
+          .text(formattedDate + ' ' + minutes + ' ' + mouseoverLabel);
       }
-    })
-    .entries(activities);
-
-    summedActivities.map(function (activity) {
-      // Create date and duration attributes with proper data types
-      activity.date = new Date(activity.key);
-      activity.duration = parseInt(activity.values.duration);
-
-      // Delete unused key and values
-      delete activity.values
-      delete activity.key;
-    })
-
-  // Get translation strings
-  const activityTrendTitle = TAPi18n.__("residentActivityTrendChart-title");
-  const activityTrendDescription = TAPi18n.__("residentActivityTrendChart-description");
-  const yLabel = TAPi18n.__("residentActivityTrendChart-yLabel");
-  const mouseoverLabel = TAPi18n.__("residentActivityTrendChart-mouseoverLabel");
-
-  // Render the chart using MetricsGraphics charting library
-  MG.data_graphic({
-    title: activityTrendTitle,
-    description: activityTrendDescription,
-    y_label: yLabel,
-    y_rug: true,
-    data: summedActivities,
-    chart_type: 'point',
-    least_squares: true,
-    full_width: true,
-    height: 200,
-    right: 40,
-    target: document.getElementById('activity-chart'),
-    x_accessor: 'date',
-    y_accessor: 'duration',
-    mouseover: function (d, i) {
-      const date = d.point.date;
-      const formattedDate = moment(date).format("D.M.YYYY");
-      const minutes = d.point.duration;
-      d3.select('#activity-chart svg .mg-active-datapoint')
-        .text(formattedDate + ' ' + minutes + ' ' + mouseoverLabel);
-    }
+    });
   });
 }
