ef859fec8fd1337264526c358e7ed0a86a5efcdc
Brylie Christopher Oxley <brylie@amble.fi>
2016-10-23 11:14:45 +0300

    Create random roles; lint, comments, whitespace

--- packages/wellbeing-mock-data/wellbeing-mock-data.js
+++ packages/wellbeing-mock-data/wellbeing-mock-data.js
@@ -2,125 +2,147 @@
 
 var groups = ['Group A', 'Group B','Group C'];
 var activities = ['Outdoor recreation', 'Stretching/resistence training', 'Sensory stimulation',
-						'Pet therapy','Gardening', 'Sewing','Painting'];
+            'Pet therapy','Gardening', 'Sewing','Painting'];
 
 var facilitatorRoles = ['Caregiver', 'Volunteer', 'Family', 'Self'];
 
 function randomDate(start, end) {
-	return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 }
 
 function insert(collection, args, amount, insertFunctionOutcome) {
-	//make copy of the object so we can get different faker data every time
-	var argsWithValues;
-	for (var i = 0; i < amount; i++) {
-		 argsWithValues = Object.assign({},args);
+  //make copy of the object so we can get different faker data every time
+  var argsWithValues;
+  for (var i = 0; i < amount; i++) {
+     argsWithValues = Object.assign({},args);
 
-		for (var key in args) {
-			if (insertFunctionOutcome) {
-				if (typeof args[key] === 'function') {
-					var func = args[key];
-					argsWithValues[key] = func();
-				}
-			}
-		}
-		collection.insert(argsWithValues);
+    for (var key in args) {
+      if (insertFunctionOutcome) {
+        if (typeof args[key] === 'function') {
+          var func = args[key];
+          argsWithValues[key] = func();
+        }
+      }
+    }
+    collection.insert(argsWithValues);
 
-	}
+  }
 }
 function createMockGroups () {
-	console.log('Creating mock groups');
+  console.log('Creating mock groups');
 
-	for (var i = 0; i < groups.length; i++) {
-		var groupName = groups[i];
-		console.log(groupName);
-		Groups.insert({'name': groupName});
-	}
+  for (var i = 0; i < groups.length; i++) {
+    var groupName = groups[i];
+    console.log(groupName);
+    Groups.insert({'name': groupName});
+  }
 }
 
 function createMockRoles() {
-	console.log('Creating mock roles');
+  console.log('Creating mock roles');
 
-	for (var i = 0; i < facilitatorRoles.length; i++) {
-		var roleName = facilitatorRoles[i];
-		console.log(roleName);
-		Roles.createRole(roleName);
-	}
+  for (var i = 0; i < facilitatorRoles.length; i++) {
+    var roleName = facilitatorRoles[i];
+    console.log(roleName);
+    Roles.createRole(roleName);
+  }
 }
 
 function createMockHomes() {
-	console.log('Creating mock homes');
-	var amount = 3;
-	var address = function() {return faker.address.city();};
+  console.log('Creating mock homes');
+  var amount = 3;
+  var address = function() {return faker.address.city();};
 
-	for (var i = 0; i < groups.length; i++) {
-		var groupName = groups[i];
-		console.log(groupName);
-		var Group = Groups.findOne({'name': groupName});
-		var args = {'name': address,'groupId': Group._id};
-		insert(Homes, args, amount, true);
-	}
+  for (var i = 0; i < groups.length; i++) {
+    var groupName = groups[i];
+    console.log(groupName);
+    var Group = Groups.findOne({'name': groupName});
+    var args = {'name': address,'groupId': Group._id};
+    insert(Homes, args, amount, true);
+  }
 
 }
 
 function createMockResidents() {
-	console.log('Creating mock residents');
-	var amount = 6;
-	var homes = Homes.find().fetch();
-	var firstName = function() {return faker.name.firstName();};
-	var lastInitial =  function() {return faker.name.lastName().charAt(0);};
-	for (var i = 0; i < homes.length; i++) {
-		var args = {'firstName': firstName, 'lastInitial': lastInitial, 'homeId': homes[i]._id};
-		insert(Residents, args, amount,true);
-	}
+  console.log('Creating mock residents');
+  var amount = 6;
+  var homes = Homes.find().fetch();
+  var firstName = function() {return faker.name.firstName();};
+  var lastInitial =  function() {return faker.name.lastName().charAt(0);};
+  for (var i = 0; i < homes.length; i++) {
+    var args = {'firstName': firstName, 'lastInitial': lastInitial, 'homeId': homes[i]._id};
+    insert(Residents, args, amount,true);
+  }
 
 }
 function createMockActivityTypes() {
-	console.log('Creating mock activiity types');
-	for (var i = 0; i < activities.length; i++) {
-		ActivityTypes.insert({'name': activities[i]});
-	}
+  console.log('Creating mock activiity types');
+  for (var i = 0; i < activities.length; i++) {
+    ActivityTypes.insert({'name': activities[i]});
+  }
 }
 
 function createMockActivities() {
-	console.log('Creating mock activies');
-	var amount =  25;
+  console.log('Creating mock activies');
+  // Number of activities to create per resident
+  var amount =  25;
 
-	var residents = Residents.find().fetch();
-	var activityTypes = ActivityTypes.find().fetch();
-	var roleId = Roles.getAllRoles().fetch()[0]._id;
-	var activityType = function() {return activityTypes[Math.floor((Math.random() * activityTypes.length) + 0)]._id;};
-	var date = function() {
+  // Number, in days, for the earliest activity date
+  var earliestActivityDate = 30;
 
-		var endDate = new Date();
-		var startDate = new Date();
-		startDate.setDate(endDate.getDate() - 30);
-	return randomDate(endDate,startDate);};
-	var duration = function() {return Math.floor((Math.random() * 60) + 1);};
-	for (var i = 0; i < residents.length; i++) {
+  // Get list of residents
+  var residents = Residents.find().fetch();
 
-		var residentId = residents[i]._id;
-		var args = {'activityTypeId': activityType, 'activityDate': date,'facilitatorRoleId': roleId,'duration': duration,'residentIds': [residentId]};
-		insert(Activities,args,amount,true);
-	}
+  // Get list of activity types
+  var activityTypes = ActivityTypes.find().fetch();
+
+  // Get a list of all roles
+  var roles = Roles.getAllRoles.fetch();
+
+  // Select a random facilitator role (ID)
+  var roleId = function () {
+    return roles[Math.floor(Math.random() * roles.length)]._id;
+  }
+
+  // Get a random activity type (ID)
+  var activityType = function() {
+    return activityTypes[Math.floor(Math.random() * activityTypes.length)]._id;
+  };
+
+  // Get random activity date
+  var date = function() {
+    var endDate = new Date();
+    var startDate = new Date();
+    startDate.setDate(endDate.getDate() - earliestActivityDate);
+    return randomDate(endDate, startDate);
+  };
+
+  // Set random activity duration
+  var duration = function() {
+    return Math.floor((Math.random() * 60) + 1);
+  };
+
+  for (var i = 0; i < residents.length; i++) {
+    var residentId = residents[i]._id;
+    var args = {'activityTypeId': activityType, 'activityDate': date,'facilitatorRoleId': roleId,'duration': duration,'residentIds': [residentId]};
+    insert(Activities, args, amount, true);
+  }
 }
 
 Meteor.methods({
-	'createMockData': function() {
-		createMockGroups();
-		createMockRoles();
-		createMockHomes();
-		createMockResidents();
-		createMockActivityTypes();
-		createMockActivities();
-	},
-	'removeAllData': function() {
-		Groups._dropCollection();
-		Homes._dropCollection();
-		Residents._dropCollection();
-		ActivityTypes._dropCollection();
-		Activities._dropCollection();
-
-	},
-
+  'createMockData': function() {
+    createMockGroups();
+    createMockRoles();
+    createMockHomes();
+    createMockResidents();
+    createMockActivityTypes();
+    createMockActivities();
+  },
+  'removeAllData': function() {
+    Groups._dropCollection();
+    Homes._dropCollection();
+    Residents._dropCollection();
+    ActivityTypes._dropCollection();
+    Activities._dropCollection();
+  },
 });
