d1915effde1dc168155e103c8c05227663fce0de
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-11-26 13:48:18 +0200

    Move autoform code to autoform.js

--- client/views/activity/new/autoform.js
+++ client/views/activity/new/autoform.js
@@ -11,3 +11,18 @@ AutoForm.addHooks(['newActivityForm'], {
     FlashMessages.sendSuccess(successMessageWithIcon);
   }
 });
+
+Template.autoForm.onRendered(function () {
+  const instance = this;
+
+  // Make sure the new activity form is being rendered
+  if (instance.data.id === "newActivityForm") {
+    // Get a reference to the resident select field
+    const residentSelect = $('[name=residentIds]');
+
+    // Activate the improved multi-select widget
+    residentSelect.selectpicker({
+      header: "Select resident(s)"
+    });
+  }
+});
--- client/views/activity/new/newActivity.js
+++ client/views/activity/new/newActivity.js
@@ -4,19 +4,3 @@ Template.newActivity.created = function () {
   this.subscribe('allActivityTypes');
   this.subscribe('allRolesExceptAdmin');
 };
-
-
-Template.autoForm.onRendered(function () {
-  const instance = this;
-
-  // Make sure the new activity form is being rendered
-  if (instance.data.id === "newActivityForm") {
-    // Get a reference to the resident select field
-    const residentSelect = $('[name=residentIds]');
-
-    // Activate the improved multi-select widget
-    residentSelect.selectpicker({
-      header: "Select resident(s)"
-    });
-  }
-});
