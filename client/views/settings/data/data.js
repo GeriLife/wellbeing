23da36fb3719b1cfe9ae812d2cab3621ae50a1d9
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-03-04 16:27:56 +0200

    Initial click #export-data function

--- client/views/settings/data/data.js
+++ client/views/settings/data/data.js
@@ -1,4 +1,12 @@
 Template.dataSettings.events({
+  'click #export-data' () {
+    // Export all data
+    Meteor.call('exportAllData', function (error, exportData) {
+      if (!error) {
+        console.log(exportData);
+      }
+    });
+  },
   'click #generate-demo-data' () {
     // Create demo data via server method
     Meteor.call('createMockData');
