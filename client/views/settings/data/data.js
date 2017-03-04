422ea1b0557c39ccbae66ab51e6d749a6cc437cf
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-03-04 17:07:48 +0200

    UI state to indicate fetching data

--- client/views/settings/data/data.js
+++ client/views/settings/data/data.js
@@ -1,7 +1,18 @@
 import FileSaver from 'file-saver';
 
+Template.dataSettings.onCreated(function () {
+  // Get reference to template instance
+  const templateInstance = this;
+
+  // 'fetching data' variable for UI state
+  templateInstance.fetchingData = new ReactiveVar(false);
+});
+
 Template.dataSettings.events({
-  'click #export-data' () {
+  'click #export-data' (event, templateInstance) {
+    // Set 'fetching data' variable to true
+    templateInstance.fetchingData.set(true);
+
     // Export all data
     Meteor.call('exportAllData', function (error, exportData) {
       if (!error) {
@@ -13,6 +24,9 @@ Template.dataSettings.events({
 
         // Trigger save as prompt
         FileSaver.saveAs(exportBlob, 'GeriLife-export.json');
+
+        // hide spinner
+        templateInstance.fetchingData.set(false);
       }
     });
   },
@@ -21,3 +35,13 @@ Template.dataSettings.events({
     Meteor.call('createMockData');
   }
 });
+
+Template.dataSettings.helpers({
+  fetchingData () {
+    // Get reference to template instance
+    const templateInstance = Template.instance();
+
+    // return current value of 'fetchingData'
+    return templateInstance.fetchingData.get();
+  }
+})
