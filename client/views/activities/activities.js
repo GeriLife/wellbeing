b825bfab4b8c962934e13ddba6fefb161ff99fab
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-03-04 17:55:26 +0200

    Comment out unneeded code, for now

--- client/views/activities/activities.js
+++ client/views/activities/activities.js
@@ -57,48 +57,48 @@ Template.activities.events({
 });
 
 Template.activities.helpers({
-  "activities": function () {
-    // Query Activities collection for all activities
-    var activities = Activities.find().fetch();
-
-    // Create a placeholder array for resident objects
-    var activitiesArray = [];
-
-    // Iterate through activities
-    activities.forEach(function (activity) {
-      // Get all resident IDs
-      var residentIds = activity.residentIds;
-
-      // Placeholder for resident names
-      var homeNames = _.map(residentIds, function (residentId) {
-        // Get the resident
-        var resident = Residents.findOne(residentId);
-
-        // Get resident home name
-        var homeName = resident.homeName();
-
-        return homeName;
-      });
-
-      // Get only unique home names
-      var homeNamesUnique = _.uniq(homeNames);
-
-      // set resident names, type, duration, and date values
-      var activityObject = {
-        activityId: activity._id,
-        residents: activity.residentNames(),
-        type: activity.activityType(),
-        duration: activity.duration,
-        activityDate: activity.activityDate,
-        homeNames: homeNamesUnique
-      };
-
-      // Add activity object to residents list
-      activitiesArray.push(activityObject);
-    });
-
-    return activitiesArray;
-  },
+  // "activities": function () {
+  //   // Query Activities collection for all activities
+  //   var activities = Activities.find().fetch();
+  //
+  //   // Create a placeholder array for resident objects
+  //   var activitiesArray = [];
+  //
+  //   // Iterate through activities
+  //   activities.forEach(function (activity) {
+  //     // Get all resident IDs
+  //     var residentIds = activity.residentIds;
+  //
+  //     // Placeholder for resident names
+  //     var homeNames = _.map(residentIds, function (residentId) {
+  //       // Get the resident
+  //       var resident = Residents.findOne(residentId);
+  //
+  //       // Get resident home name
+  //       var homeName = resident.homeName();
+  //
+  //       return homeName;
+  //     });
+  //
+  //     // Get only unique home names
+  //     var homeNamesUnique = _.uniq(homeNames);
+  //
+  //     // set resident names, type, duration, and date values
+  //     var activityObject = {
+  //       activityId: activity._id,
+  //       residents: activity.residentNames(),
+  //       type: activity.activityType(),
+  //       duration: activity.duration,
+  //       activityDate: activity.activityDate,
+  //       homeNames: homeNamesUnique
+  //     };
+  //
+  //     // Add activity object to residents list
+  //     activitiesArray.push(activityObject);
+  //   });
+  //
+  //   return activitiesArray;
+  // },
   'tableSettings': function () {
     var tableSettings = {
       showFilter: false,
@@ -107,8 +107,7 @@ Template.activities.helpers({
         {
           key: 'residents',
           label: 'Resident(s)',
-          sortOrder: 1,
-          sortDirection: 'ascending'
+          tmpl: Template.activitiesTableResidentsCell,
         },
         {
           key: 'homeNames',
