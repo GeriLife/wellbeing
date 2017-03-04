4691c151db69e2880092ee65a1ffd501936bacdf
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-03-04 16:36:52 +0200

    Initial export file handling

--- client/views/settings/data/data.js
+++ client/views/settings/data/data.js
@@ -1,9 +1,18 @@
+import FileSaver from 'file-saver';
+
 Template.dataSettings.events({
   'click #export-data' () {
     // Export all data
     Meteor.call('exportAllData', function (error, exportData) {
       if (!error) {
-        console.log(exportData);
+        // Convert export data to string
+        const dataString = JSON.stringify(exportData)
+
+        // Create binary object from data string
+        const exportBlob = new Blob([dataString], {type: 'application/json;charset=utf-8'});
+
+        // Trigger save as prompt
+        FileSaver.saveAs(exportBlob, 'GeriLife-export.json');
       }
     });
   },
