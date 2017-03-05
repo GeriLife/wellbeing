b4a75d42bf696faf5212af6e55727030e81bf8e4
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-03-05 18:56:53 +0200

    Cleanup

--- client/views/activities/activities.js
+++ client/views/activities/activities.js
@@ -22,48 +22,6 @@ Template.activities.events({
 });
 
 Template.activities.helpers({
-  // "activities": function () {
-  //   // Query Activities collection for all activities
-  //   var activities = Activities.find().fetch();
-  //
-  //   // Create a placeholder array for resident objects
-  //   var activitiesArray = [];
-  //
-  //   // Iterate through activities
-  //   activities.forEach(function (activity) {
-  //     // Get all resident IDs
-  //     var residentIds = activity.residentIds;
-  //
-  //     // Placeholder for resident names
-  //     var homeNames = _.map(residentIds, function (residentId) {
-  //       // Get the resident
-  //       var resident = Residents.findOne(residentId);
-  //
-  //       // Get resident home name
-  //       var homeName = resident.homeName();
-  //
-  //       return homeName;
-  //     });
-  //
-  //     // Get only unique home names
-  //     var homeNamesUnique = _.uniq(homeNames);
-  //
-  //     // set resident names, type, duration, and date values
-  //     var activityObject = {
-  //       activityId: activity._id,
-  //       residents: activity.residentNames(),
-  //       type: activity.activityType(),
-  //       duration: activity.duration,
-  //       activityDate: activity.activityDate,
-  //       homeNames: homeNamesUnique
-  //     };
-  //
-  //     // Add activity object to residents list
-  //     activitiesArray.push(activityObject);
-  //   });
-  //
-  //   return activitiesArray;
-  // },
   'tableSettings': function () {
     var tableSettings = {
       showFilter: false,
@@ -121,13 +79,6 @@ Template.activities.helpers({
   "residentFilterLabel": function () {
     // Get internationalized resident filter string
     return TAPi18n.__("activities-filterActivities-residentFilterLabel");
-  },
-  'homeFilterFields': function () {
-    return ['homeNames'];
-  },
-  "homeFilterLabel": function () {
-    // Get internationalized resident filter string
-    return TAPi18n.__("activities-filterActivities-homeFilterLabel");
   },
   'activityTypeFilterFields': function () {
     return ['type'];
