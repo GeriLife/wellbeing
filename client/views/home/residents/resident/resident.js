5db784df742c35c09438e45195db950c701d570f
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-04-02 19:44:56 +0300

    Indicate when resident is on hiatus

--- client/views/home/residents/resident/resident.js
+++ client/views/home/residents/resident/resident.js
@@ -20,6 +20,14 @@ Template.homeResident.helpers({
     // Get reference to template instance
     var instance = Template.instance();
 
+    // Get resident
+    const resident = instance.data;
+
+    // Check if resident is on hiatus
+    if (resident.onHiatus) {
+      return 'info';
+    }
+
     // Get resident activity count
     var activityCount = instance.residentActivityCount.get();
 
@@ -37,6 +45,14 @@ Template.homeResident.helpers({
     var instance = Template.instance();
 
     // Map the resident activity level to a Bootstrap class
+
+    // Get resident
+    const resident = instance.data;
+
+    // Check if resident is on hiatus
+    if (resident.onHiatus) {
+      return 'on hiatus';
+    }
 
     // Get resident ID from template instance
     var residentId = instance.residentId;
