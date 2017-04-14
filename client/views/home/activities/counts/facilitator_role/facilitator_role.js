a2a2a946f6739fb3eea35b62a0345771c5b84578
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-04-14 18:16:38 +0300

    Add initial Activity Counts by Facilitator Role template

---
+++ client/views/home/activities/counts/facilitator_role/facilitator_role.js
@@ -0,0 +1,9 @@
+Template.homeActivityCountsByFacilitatorRole.onCreated(function () {
+  // Set up reactive variable for chart data
+
+  // Call method to fetch data, assigning it to reactive variable
+});
+
+Template.homeActivityCountsByFacilitatorRole.onCreated(function () {
+  // Autorun to render chart when reactive data available
+});
