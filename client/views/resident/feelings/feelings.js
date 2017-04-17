a82dd4560fd95dce97a8728ae37186ee3e194c0b
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-04-17 21:17:58 +0300

    Initial working chart logic

--- client/views/resident/feelings/feelings.js
+++ client/views/resident/feelings/feelings.js
@@ -2,3 +2,42 @@ Template.residentFeelings.onRendered(function () {
   // Subscribe to resident feelings for current residentID
   this.subscribe('residentFeelings', this.data.residentId);
 });
+
+Template.residentFeelings.onRendered(function () {
+  // Get resident ID
+  const residentId = this.data.residentId;
+
+  this.autorun(() => {
+    // Make sure subscription data is ready
+    if (this.subscriptionsReady()) {
+      // Get all feelings
+      const residentFeelings = Feelings.find({ residentId }).fetch();
+
+      // Group feelings by type and calculate percentage for each feeling
+      const residentFeelingsCounts = d3.nest()
+        .key(function (datum) { return datum.feeling })
+        .rollup(function (values) { return values.length / residentFeelings.length })
+        .entries(residentFeelings);
+
+      // Make sure feeling counts are available
+      if (residentFeelingsCounts) {
+        // clear any previous chart
+        this.$('#residentFeelingsChart').empty();
+
+        // Render chart with current feeling counts
+        MG.data_graphic({
+          title: 'Feelings',
+          data: residentFeelingsCounts,
+          chart_type: 'bar',
+          x_accessor: 'values',
+          format: 'percentage',
+          xax_format: d3.format('.0%'),
+          y_accessor: 'key',
+          height:300,
+          full_width: true,
+          target: '#residentFeelingsChart',
+        });
+      }
+    }
+  });
+});
