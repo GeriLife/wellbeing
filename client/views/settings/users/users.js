458864b0f235c4e0ebfd659b7b86722ea3f73922
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-05-14 13:36:17 +0300

    Make sure user can't delete own account

--- client/views/settings/users/users.js
+++ client/views/settings/users/users.js
@@ -24,6 +24,25 @@ Template.usersSettings.helpers({
       // Return comma separated list of roles
       return this.roles.join();
     }
+  },
+  "userCanDeleteAccount": function () {
+    // Prevent user from deleting own account
+    // by hiding delete button
+
+    // get logged in user ID
+    const loggedInUserId = Meteor.userId();
+
+    // get users table user id
+    const tableUserId = this._id;
+
+    if (loggedInUserId !== tableUserId) {
+      // logged in user different from table user
+      // so, allow delete
+      return true;
+    }
+
+    // otherwise, don't allow delete (hide button)
+    return false;
   }
 });
 
