3a8ebb82e964b2deadfe5f216e6a54c0a847f482
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-05-21 17:16:55 +0300

    Add update allow rule; refactor insert rule

--- both/collections/groups.js
+++ both/collections/groups.js
@@ -24,7 +24,28 @@ Groups.helpers({
 });
 
 Groups.allow({
-  insert: function () {
-    return true;
+  insert () {
+    // Get user ID
+    const userId = Meteor.userId();
+
+    // Check if user is administrator
+    const userIsAdmin = Roles.userIsInRole(userId, ['admin']);
+
+    if (userIsAdmin) {
+      // admin user can insert
+      return true;
+    }
+  },
+  update () {
+    // Get user ID
+    const userId = Meteor.userId();
+
+    // Check if user is administrator
+    const userIsAdmin = Roles.userIsInRole(userId, ['admin']);
+
+    if (userIsAdmin) {
+      // admin user can update
+      return true;
+    }
   }
 });
