4360ea7e3550eaa7de8c630c072a8a7e352cff57
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-06-04 20:35:49 +0300

    Cleanup console logs

--- client/views/activities/manage/delete/confirm/modal.js
+++ client/views/activities/manage/delete/confirm/modal.js
@@ -1,9 +1,8 @@
 Template.deleteActivityConfirmation.events({
   "click #confirm-delete" () {
-    console.log(this);
     // Get Activity ID
     const activityId = this.activity._id;
-    console.log(activityId);
+
     // Delete Activity
     Activities.remove(activityId, function () {
       // Dismiss the modal dialogue
