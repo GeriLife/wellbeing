22137b23e5a780478667d28e2b4f051923d3f8cd
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-06-11 16:47:00 +0300

    Add subject field

--- client/views/settings/users/enroll/schema.js
+++ client/views/settings/users/enroll/schema.js
@@ -1,4 +1,10 @@
 const usersEnrollSchema = new SimpleSchema({
+  subject: {
+    type: String,
+    // suggested maximum length of email subject
+    // https://stackoverflow.com/a/1592310/1191545
+    max: 78
+  }
   message: {
     type: String,
     optional: false
