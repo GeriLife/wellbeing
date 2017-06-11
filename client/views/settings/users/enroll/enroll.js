8782fdcd28f99c79df679a7818b4fb079f1a6103
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-06-11 16:59:25 +0300

    add i18n for usersEnroll submit button

--- client/views/settings/users/enroll/enroll.js
+++ client/views/settings/users/enroll/enroll.js
@@ -1,6 +1,12 @@
 import usersEnrollSchema from './schema';
 
 Template.usersEnroll.helpers({
+  buttonContent () {
+    // Get localized text for send buttonContent
+    const buttonContent = TAPi18n.__('usersEnroll_form_submitButton_text');
+
+    return buttonContent;
+  },
   usersEnrollSchema () {
     return usersEnrollSchema;
   }
