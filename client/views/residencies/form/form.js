47804412fa4ee03e787c5358c914c87429f4efb5
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-08-28 17:02:19 +0300

    Add logic to toggle form based on new or existing user

--- client/views/residencies/form/form.js
+++ client/views/residencies/form/form.js
@@ -10,11 +10,40 @@ Template.residencyForm.onCreated(function () {
 
   // Subscribe to all groups, so homes can be grouped in select menu
   templateInstance.subscribe('allGroups');
+
+  // Create reactive variable to hold state of 'new or existing resident' radio
+  // initialize to 'existing' to match template markup
+  templateInstance.newOrExistingResident = new ReactiveVar('existing');
 });
 
 Template.residencyForm.helpers({
-  'today': function () {
+  today () {
     // Default date today, as a string
     return Date();
   },
+  addingResidencyForExistingUser () {
+    // Get reference to template instance
+    const templateInstance = Template.instance();
+
+    // Get value of 'new or existing resident' reactive variable
+    const newOrExistingResident = templateInstance.newOrExistingResident.get();
+
+    if (newOrExistingResident === 'existing') {
+      // adding residency for existing user is true
+      return true;
+    }
+
+    // otherwise return false
+    return false;
+  }
+});
+
+Template.residencyForm.events({
+  'change [name=new-or-existing]' (event, templateInstance) {
+    // Get selected value of 'new or existing resident' radio
+    const newOrExistingResident = event.target.value;
+
+    // Update 'new or existing resident' reactive variable
+    templateInstance.newOrExistingResident.set(newOrExistingResident);
+  }
 });
