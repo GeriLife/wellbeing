d97b6ecefd4cd38dc17e08d9ffdfd27775daed26
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-08-31 19:25:29 +0300

    Hide modal dialogue on success

---
+++ client/views/residencies/form/autoform.js
@@ -0,0 +1,11 @@
+AutoForm.hooks({
+  'addNewResidentAndResidency': {
+    onSuccess (formType, success) {
+      // check if submission was successful
+      if (success) {
+        // Hide modal dialogue
+        Modal.hide('residencyForm');
+      }
+    },
+  },
+});
