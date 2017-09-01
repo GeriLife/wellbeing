4c7eb0da44d2cc7b4d098f6fb1bc2301a338180f
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-09-01 09:26:49 +0300

    Add .edit-residency click handler

--- client/views/residents/residency/residency.js
+++ client/views/residents/residency/residency.js
@@ -1,6 +1,18 @@
 Template.residentCurrentResidency.onCreated(function () {
   // Get resident ID
   const residentId = this.residentId;
-  
+
   console.log(this);
 })
+
+
+Template.residentCurrentResidency.events({
+  'click .edit-residency' (event, templateInstance) {
+    // Get resident and home ID
+    const residentId = templateInstance.residentId;
+    const homeId = templateInstance.homeId;
+
+    // Show the edit residency modal
+    Modal.show('residencyForm'{ residentId, homeId })
+  }
+});
