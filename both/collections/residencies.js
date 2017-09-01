4d7b52a67607f376142525bcc2927d8899f43996
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-09-01 10:34:28 +0300

    Allow admin users insert, update, remove

--- both/collections/residencies.js
+++ both/collections/residencies.js
@@ -103,3 +103,66 @@ var ResidenciesSchema = new SimpleSchema({
 ResidenciesSchema.i18n("residencies");
 
 Residencies.attachSchema(ResidenciesSchema);
+
+Residencies.allow({
+  'insert': function () {
+    // Get user ID
+    let userId = Meteor.userId();
+
+    // Placeholder for administrator check
+    let userIsAdministrator;
+
+    // Placeholder for insert privilege check
+    let userCanInsert;
+
+    if (userId) {
+      // Check if user is administrator
+      userIsAdministrator = Roles.userIsInRole(userId, ['admin']);
+    }
+
+    // Only allow adminstator users insert
+    userCanInsert = (userId && userIsAdministrator);
+
+    return userCanInsert;
+  },
+  'update': function () {
+    // Get user ID
+    let userId = Meteor.userId();
+
+    // Placeholder for administrator check
+    let userIsAdministrator;
+
+    // Placeholder for update privilege check
+    let userCanUpdate;
+
+    if (userId) {
+      // Check if user is administrator
+      userIsAdministrator = Roles.userIsInRole(userId, ['admin']);
+    }
+
+    // Only allow adminstator users insert
+    userCanUpdate = (userId && userIsAdministrator);
+
+    return userCanUpdate;
+  },
+  'remove': function () {
+    // Get user ID
+    let userId = Meteor.userId();
+
+    // Placeholder for administrator check
+    let userIsAdministrator;
+
+    // Placeholder for insert privilege check
+    let userCanRemove;
+
+    if (userId) {
+      // Check if user is administrator
+      userIsAdministrator = Roles.userIsInRole(userId, ['admin']);
+    }
+
+    // Only allow adminstator users insert
+    userCanRemove = (userId && userIsAdministrator);
+
+    return userCanRemove;
+  }
+});
