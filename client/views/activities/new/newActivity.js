f7cc6d670511e03df1ecd7693b075fd826ef0e6c
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-09-04 14:04:57 +0300

    Add select2Options helper

--- client/views/activities/new/newActivity.js
+++ client/views/activities/new/newActivity.js
@@ -1,3 +1,7 @@
+import 'select2';
+import 'select2/dist/css/select2.css';
+import 'select2-bootstrap-theme/dist/select2-bootstrap.css';
+
 Template.newActivity.created = function () {
   this.subscribe('allCurrentResidents');
   this.subscribe('allHomes');
@@ -6,6 +10,16 @@ Template.newActivity.created = function () {
 };
 
 Template.newActivity.helpers({
+  select2Options () {
+    // Get placeholder text localization
+    const placeholderText = TAPi18n.__('newActivity-residentSelect-placeholder');
+
+    const options = {
+      placeholder: 'foo',
+    };
+
+    return options;
+  },
   'today': function () {
     // Default date today, as a string
     return Date();
