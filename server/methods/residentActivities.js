3c76b4d1b1ef536ca70f9f0262a9e2e98d9e2964
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-11-25 12:22:51 +0200

    Add checkIfResidentWasAtiveOnDate method

--- server/methods/residentActivities.js
+++ server/methods/residentActivities.js
@@ -1,6 +1,33 @@
 Meteor.methods({
   checkIfResidentWasActiveOnDate (residentId, date) {
+    // Create morning and evening dates
+    // in order to query activities for date
+    const morning = new Date(date);
+    morning.setHours(0);
+    morning.setMinutes(0);
+    morning.setSeconds(0);
 
+    const evening = new Date(date);
+    evening.setHours(23);
+    evening.setMinutes(59);
+    evening.setSeconds(59);
+
+    // Check if resident has (at least one) recorded activity on that day (date)
+    const residentActiveOnDate = Activities.findOne({
+      // Make sure resident ID in resident IDs array
+      residentIds: {
+        $elemMatch: { $eq: residentId }
+      },
+      // Make sure activity recorded on desired date
+      activityDate: {
+        $gte: morning,
+        $lte: evening,
+      }
+    });
+    console.log(residentActiveOnDate)
+    // Return the (boolean) value indicating whether resident was active on date
+    // note: we make sure we got a document back by comparing against undefined
+    return (residentActiveOnDate != undefined);
   },
   'getResidentRecentActivitiesCount': function (residentId) {
     // Date one week ago
