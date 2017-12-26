4c040d07141774f585e8abc3b04caff475a17762
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-12-26 20:10:56 +0200

    Cleanup

--- app/server/methods/residents.js
+++ app/server/methods/residents.js
@@ -56,23 +56,6 @@ Meteor.methods({
 
     return residentsLatestActivityByType;
   },
-  'getResidentsWithoutActivityByType': function (activityTypeId) {
-    // // Get resident latest activity by type
-    // var residentLastActivityByType = Meteor.call('getResidentsLatestActivityByType', activityTypeId);
-    //
-    // // Get all resident IDs
-    // var residentIds = Residents.find({}, {fields: {_id: 1}}).fetch();
-    //
-    // // Get residents who have participated in the activity type
-    // var residentsWithPreviousActivity = _.map(residentLastActivityByType, function (resident) {
-    //   return resident._id;
-    // });
-    //
-    // // Get residents who have not participated in activity type
-    // var residentsWithoutPreviousActivity = _.difference(residentIds, residentsWithPreviousActivity);
-    //
-    // return residentsWithoutPreviousActivity;
-  },
   'getResidentRecentActivities': function (residentId) {
     // Date two weeks ago
     var twoWeeksAgo = moment().subtract(2, "weeks").toDate();
