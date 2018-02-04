692be00d50992b70a4e3093b010d75b840bdab1f
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-02-04 15:10:17 +0200

    Field indexes only on server

--- app/both/collections/activities.js
+++ app/both/collections/activities.js
@@ -3,11 +3,13 @@ import SimpleSchema from 'simpl-schema';
 Activities = new Mongo.Collection('activities');
 
 Meteor.startup(function () {
-  // Make sure activityDate field is indexed for performance
-  Activities._ensureIndex({'activityDate': 1});
+  if(Meteor.isServer) {
+    // Make sure activityDate field is indexed for performance
+    Activities._ensureIndex({'activityDate': 1});
 
-  // Make sure residentIds field is indexed for performance
-  Activities._ensureIndex({'residentIds': 1});
+    // Make sure residentIds field is indexed for performance
+    Activities._ensureIndex({'residentIds': 1});
+  }
 });
 
 var ActivitiesSchema = new SimpleSchema({
