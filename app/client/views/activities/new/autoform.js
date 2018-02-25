ac0f7c95dda8e525273184c3626f135ee8a84ef9
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-02-25 15:16:12 +0200

    Add slim-select widget

--- app/client/views/activities/new/autoform.js
+++ app/client/views/activities/new/autoform.js
@@ -1,3 +1,6 @@
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.min.css';
+
 AutoForm.addHooks(['newActivityForm'], {
   'onSuccess': function () {
     // Hide the modal dialogue
@@ -17,12 +20,14 @@ Template.autoForm.onRendered(function () {
 
   // Make sure the new activity form is being rendered
   if (instance.data.id === "newActivityForm") {
-    // Get a reference to the resident select field
-    const residentSelect = $('[name=residentIds]');
+    // Get localized placeholder text
+    const placeholder = TAPi18n.__('newActivity-residentSelect-placeholder');
 
-    // Activate the improved multi-select widget
-    residentSelect.selectpicker({
-      header: "Select resident(s)"
+    // Render multi-select widget on 'select residents' field
+    new SlimSelect({
+      select: '[name=residentIds]',
+      closeOnSelect: false,
+      placeholder,
     });
   }
 });
