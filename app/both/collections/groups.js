cc865a3089a4d6b0f0e51b15da3b6f629c6942b0
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-03-11 19:31:57 +0200

    Add event logging to all collections

--- app/both/collections/groups.js
+++ app/both/collections/groups.js
@@ -48,3 +48,33 @@ Groups.allow({
     }
   }
 });
+
+Groups.after.insert(function (userId, group) {
+  // Add event log
+  UserEventLog.insert({
+    userId,
+    action: 'insert',
+    entityType: 'group',
+    entityId: group._id,
+  })
+});
+
+Groups.after.update(function (userId, group) {
+  // Add event log
+  UserEventLog.insert({
+    userId,
+    action: 'update',
+    entityType: 'group',
+    entityId: group._id,
+  })
+});
+
+Groups.after.remove(function (userId, group) {
+  // Add event log
+  UserEventLog.insert({
+    userId,
+    action: 'remove',
+    entityType: 'group',
+    entityId: group._id,
+  })
+});
--- app/both/collections/homes.js
+++ app/both/collections/homes.js
@@ -34,3 +34,33 @@ Homes.allow({
     return true;
   }
 });
+
+Homes.after.insert(function (userId, home) {
+  // Add event log
+  UserEventLog.insert({
+    userId,
+    action: 'insert',
+    entityType: 'home',
+    entityId: home._id,
+  })
+});
+
+Homes.after.update(function (userId, home) {
+  // Add event log
+  UserEventLog.insert({
+    userId,
+    action: 'update',
+    entityType: 'home',
+    entityId: home._id,
+  })
+});
+
+Homes.after.remove(function (userId, home) {
+  // Add event log
+  UserEventLog.insert({
+    userId,
+    action: 'remove',
+    entityType: 'home',
+    entityId: home._id,
+  })
+});
