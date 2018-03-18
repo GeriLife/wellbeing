39ac22b30e22f90f37974ab2ad18dfd631db24a5
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-03-18 12:48:30 +0200

    Add collection permissions (allow/deny)

--- app/both/collections/userEventLog.js
+++ app/both/collections/userEventLog.js
@@ -29,3 +29,42 @@ const UserEventLogSchema = new SimpleSchema({
 });
 
 UserEventLog.attachSchema(UserEventLogSchema);
+
+UserEventLog.allow({
+  insert: function () {
+    // Make sure a user is signed in
+    if (Meteor.userId()) {
+      return true;
+    }
+
+    return false;
+  },
+  update: function () {
+    // Get current user ID
+    var currentUserId = Meteor.userId();
+
+    // Check if current user has Admin role
+    var currentUserIsAdmin = Roles.userIsInRole(currentUserId, ["admin"]);
+
+    // Only show edit column for users with Admin role
+    if (currentUserIsAdmin) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  remove: function () {
+    // Get current user ID
+    var currentUserId = Meteor.userId();
+
+    // Check if current user has Admin role
+    var currentUserIsAdmin = Roles.userIsInRole(currentUserId, ["admin"]);
+
+    // Only show edit column for users with Admin role
+    if (currentUserIsAdmin) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+});
