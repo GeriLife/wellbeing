634c070a1db328eb79b50032d71f1ca721ed1d96
Erik Pellikka <erik@pellikka.org>
2018-04-16 18:05:00 +0300

    change var to const

--- app/server/methods/homes.js
+++ app/server/methods/homes.js
@@ -1,5 +1,3 @@
-import moment from 'moment';
-
 Meteor.methods({
   'getHomeResidentIds': function (homeId) {
     // Get all residents of specific home
@@ -50,7 +48,7 @@ Meteor.methods({
         ).fetch();
 
         // Create an array containing only resident IDs
-        var residentIds = _.map(residents, function (resident) {
+        const residentIds = _.map(residents, function (resident) {
             return resident._id;
         });
 
@@ -141,7 +139,6 @@ Meteor.methods({
 
       return residentActivityCountsByCurrentType;
     });
-    console.log(allResidentActivitySumsByType);
     return allResidentActivitySumsByType;
   },
   "getHomeActivityLevelCounts": function (homeId) {
@@ -225,38 +222,5 @@ Meteor.methods({
     };
 
     return activityCountsArray;
-  },
-  getHomeSelectOptionsWithGroups () {
-    // Get all Groups
-    var groups = Groups.find().fetch();
-
-    // Create an array of Group IDs
-    var groupIDs = _.map(groups, function (group) {
-      return group._id;
-    });
-
-    // Create select options for Homes input
-    // Grouping homes by group
-    var homeSelectOptionsWithGroups = _.map(groupIDs, function (groupId) {
-      // Find the name of this group
-      var groupName = Groups.findOne(groupId).name;
-
-      // Get all homes of this group
-      var groupHomes = Homes.find({groupId: groupId}).fetch();
-
-      // Create a homes array with name/ID pairs for label/value
-      var homesOptions = _.map(groupHomes, function (groupHome) {
-        // Combine resident first name and last initial
-        var homeName = groupHome.name;
-
-        // Create option for this home, with home ID as the value
-        return {label: homeName, value: groupHome._id};
-      });
-
-      // Return residents and home as option group
-      return {optgroup: groupName, options: homesOptions};
-    });
-
-    return homeSelectOptionsWithGroups;
-  },
+  }
 });
