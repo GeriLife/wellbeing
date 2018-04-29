492a65e71c7d571fcfe4f4a2a3199a874dcaeaf5
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-04-29 20:45:46 +0300

    Move collection index

---
+++ app/collections/activities/server/db_indexes.js
@@ -0,0 +1,7 @@
+Meteor.startup(function () {
+  // Make sure activityDate field is indexed for performance
+  Activities._ensureIndex({'activityDate': 1});
+
+  // Make sure residentIds field is indexed for performance
+  Activities._ensureIndex({'residentIds': 1});
+});
