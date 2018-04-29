1adb33452705a5515ab47c7543c005cce4d31cba
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-04-29 20:45:46 +0300

    Move collection index

--- app/collections/activities/index.js
+++ app/collections/activities/index.js
@@ -4,16 +4,6 @@ import UserEventLog from '/collections/user_event_log/';
 
 Activities = new Mongo.Collection('activities');
 
-Meteor.startup(function () {
-  if(Meteor.isServer) {
-    // Make sure activityDate field is indexed for performance
-    Activities._ensureIndex({'activityDate': 1});
-
-    // Make sure residentIds field is indexed for performance
-    Activities._ensureIndex({'residentIds': 1});
-  }
-});
-
 var ActivitiesSchema = new SimpleSchema({
   residentIds: {
     type: Array,
