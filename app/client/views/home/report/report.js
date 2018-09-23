3e84c6fde18a148f588991f76403613881335c8b
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-09-23 21:52:10 +0300

    Set reactive variable with chart data

--- app/client/views/home/report/report.js
+++ app/client/views/home/report/report.js
@@ -3,15 +3,14 @@ Template.homeReport.onCreated(function () {
   const templateInstance = this;
 
   // set reactive variable to hold chart data (attached to template instance)
-  const activityData = new ReactiveVar()
+  templateInstance.activityData = new ReactiveVar();
 
   // call method to retrieve aggregated activity data
   // Get Home ID
   templateInstance.homeId = Router.current().params.homeId;
 
   Meteor.call('getDailyAggregatedHomeResidentActivities', templateInstance.homeId, function (error, activities) {
-    console.log(activities)
+    // set activity data in the reactive variable
+    templateInstance.activityData.set(activities);
   })
-  // set activity data in the reactive variable
-  
 });
