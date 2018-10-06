360717554eca1c057c8812491d5d37d064851bc5
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-10-06 20:43:29 +0300

    Add home activity activity chart on render

--- app/client/views/home/report/report.js
+++ app/client/views/home/report/report.js
@@ -1,3 +1,6 @@
+import _ from 'lodash';
+import Plotly from 'plotly.js-dist';
+
 Template.homeReport.onCreated(function () {
   // get a reference to template instance
   const templateInstance = this;
@@ -12,5 +15,42 @@ Template.homeReport.onCreated(function () {
   Meteor.call('getDailyAggregatedHomeResidentActivities', templateInstance.homeId, function (error, activities) {
     // set activity data in the reactive variable
     templateInstance.activityData.set(activities);
+  })
+});
+
+Template.homeReport.onRendered(function () {
+  const templateInstance = this;
+
+  templateInstance.autorun(function () {
+    //TODO: add reactive data source to toggle between
+    //  - 'activity_minutes'
+    //  - 'activity_count'
+    const activityData = templateInstance.activityData.get();
+    console.log(activityData)
+
+    if (activityData) {
+      const data = _.map(activityData, function (activityCategoryData) {
+        const trace = {
+          type: 'scatter',
+          mode: 'lines',
+          name: activityCategoryData.key,
+          x: _.map(activityCategoryData.values, function(activityCategoryDay) {
+            return new Date(activityCategoryDay.key);
+          }),
+          y: _.map(activityCategoryData.values, function(activityCategoryDay) {
+            return activityCategoryDay.value.activity_minutes;
+          }),
+        }
+
+        return trace;
+      })
+
+      // TODO: add localization
+      const layout = {
+        title: 'Home resident activities',
+      }
+
+      Plotly.newPlot('homeActivityChart', data, layout)
+    }
   })
 });
