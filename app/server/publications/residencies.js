91af8a818c5112693ee78600460dd52c6e100d96
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-12-02 19:28:11 +0200

    Add allCurrentResidencies

--- app/server/publications/residencies.js
+++ app/server/publications/residencies.js
@@ -2,3 +2,10 @@ Meteor.publish('singleResidency', function (residentId, homeId) {
   // Publish a single residency
   return Residencies.find({ residentId, homeId });
 });
+
+Meteor.publish('allCurrentResidencies', function () {
+  // Find all current residencies
+  return Residencies.find({
+    moveOut: {$exists: false},
+  });
+});
