c49fc9359f72f64da3897b17360601efcd2f6738
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-12-17 04:52:33 +0200

    Render residentActivitiesChart

--- app/client/views/report/report.js
+++ app/client/views/report/report.js
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 Template.report.onCreated(function () {
   const templateInstance = this;
 
@@ -5,8 +7,55 @@ Template.report.onCreated(function () {
 
   // call method to retrieve aggregated activity data
   Meteor.call('getMonthlyAggregatedActivities', templateInstance.homeId, function (error, activityData) {
-    console.log(activityData)
     // set activity data in the reactive variable
     templateInstance.activityData.set(activityData);
+  })
+});
+
+Template.report.onRendered(function () {
+  const templateInstance = this;
+
+  templateInstance.autorun(function () {
+    //TODO: add reactive data source to toggle between
+    //  - 'activity_minutes'
+    //  - 'activity_count'
+    const activityData = templateInstance.activityData.get();
+
+    // Make sure we have some activity data
+    if (activityData) {
+      // Chart data consists of multiple traces
+      const data = _.map(activityData, function (activityCategoryData) {
+        // Create a trace for each activity type
+        const trace = {
+          type: 'bar',
+          name: activityCategoryData.key,
+          // X values are activity dates
+          x: _.map(activityCategoryData.values, function(activityCategoryDay) {
+            return new Date(activityCategoryDay.key);
+          }),
+          // Y values are (currently) activity minutes
+          // TODO: add page element to toggle between activity counts and minutes
+          y: _.map(activityCategoryData.values, function(activityCategoryDay) {
+            return activityCategoryDay.value.activity_count;
+          }),
+        }
+
+        return trace;
+      })
+
+      // Add plot layout configuration
+      const layout = {
+        title: TAPi18n.__('homeResidentsActivitiesChart-title'),
+        yaxis: {
+          title: TAPi18n.__('homeResidentsActivitiesChart-yaxis-title'),
+        }
+      }
+
+      // Get client locale
+      const locale = TAPi18n.getLanguage();
+
+      // Render plot
+      Plotly.newPlot('residentsActivitiesChart', data, layout, { locale });
+    }
   })
 });
