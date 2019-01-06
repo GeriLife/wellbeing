0b51db7bb489fa5deb86191750b5a32027ff4dd1
Brylie Christopher Oxley <brylie@gnumedia.org>
2019-01-07 00:38:52 +0200

    Create configurable resident activity overview chart
    Signed-off-by: Brylie Christopher Oxley <brylie@gnumedia.org>

--- app/client/views/resident/activities/chart/chart.js
+++ app/client/views/resident/activities/chart/chart.js
@@ -1,72 +1,107 @@
-import moment from 'moment';
+import _ from "lodash";
 
-Template.residentActivityTrendChart.rendered = function () {
+Template.residentActivityTrendChart.onCreated(function () {
+  const templateInstance = this;
+
+  // reactive variables to update chart
+  templateInstance.activityData = new ReactiveVar();
+  templateInstance.activityMetric = new ReactiveVar();
+  templateInstance.timePeriod = new ReactiveVar();
+  templateInstance.barMode = new ReactiveVar();
+});
+
+Template.residentActivityTrendChart.onRendered(function () {
   // Get reference to template instance
-  const instance = this;
+  const templateInstance = this;
 
-  // Get resident name
-  const residentName = instance.data.firstName + " " + instance.data.lastInitial;
+  // Set initial activity metric from template
+  const activityMetric = $('input[name="activityMetric"]:checked').val();
+  templateInstance.activityMetric.set(activityMetric);
 
-  instance.autorun(function () {
-    $("#activity-chart").empty();
+  // Set initial time period from template
+  const timePeriod = $('input[name="timePeriod"]:checked').val();
+  templateInstance.timePeriod.set(timePeriod);
 
-    // Get the resident activities from template instance
-    const activities = Template.currentData().activities;
+  // Set initial bar mode from template
+  const barMode = $('input[name="barMode"]:checked').val();
+  templateInstance.barMode.set(barMode);
 
-    // Group activities by activity date
-    const nestedActivities = d3.nest()
-      .key(function (activity) {
-        return activity.activityDate;
-      })
+  // automatically fetch aggregated data
+  templateInstance.autorun(function () {
+    // Get the data aggregation setting(s) from template instance
+    const timePeriod = templateInstance.timePeriod.get();
 
-    // Get a sum of activities
-    const summedActivities = nestedActivities.rollup(function (activity) {
-        return {
-          duration: d3.sum(activity, function (activity) {
-            return activity.duration;
-          })
-        }
-      })
-      .entries(activities);
+    const residentId = Template.currentData().resident._id;
 
-      summedActivities.map(function (activity) {
-        // Create date and duration attributes with proper data types
-        activity.date = new Date(activity.key);
-        activity.duration = parseInt(activity.values.duration);
-
-        // Delete unused key and values
-        delete activity.values
-        delete activity.key;
-      })
-
-    // Get translation strings
-    const activityTrendTitle = TAPi18n.__("residentActivityTrendChart-title");
-    const activityTrendDescription = TAPi18n.__("residentActivityTrendChart-description");
-    const yLabel = TAPi18n.__("residentActivityTrendChart-yLabel");
-    const mouseoverLabel = TAPi18n.__("residentActivityTrendChart-mouseoverLabel");
-
-    // Render the chart using MetricsGraphics charting library
-    MG.data_graphic({
-      title: activityTrendTitle,
-      description: activityTrendDescription,
-      y_label: yLabel,
-      y_rug: true,
-      data: summedActivities,
-      chart_type: 'point',
-      least_squares: true,
-      full_width: true,
-      height: 200,
-      right: 40,
-      target: document.getElementById('activity-chart'),
-      x_accessor: 'date',
-      y_accessor: 'duration',
-      mouseover: function (d, i) {
-        const date = d.point.date;
-        const formattedDate = moment(date).format("D.M.YYYY");
-        const minutes = d.point.duration;
-        d3.select('#activity-chart svg .mg-active-datapoint')
-          .text(formattedDate + ' ' + minutes + ' ' + mouseoverLabel);
-      }
+    // call method to retrieve aggregated activity data
+    Meteor.call('getResidentAggregatedActivities', residentId, timePeriod, function (error, activityData) {
+      // set activity data in the reactive variable
+      templateInstance.activityData.set(activityData);
     });
   });
-}
+
+  // render chart when (new) data are available
+  templateInstance.autorun(function () {
+    const activityData = templateInstance.activityData.get();
+    const activityMetric = templateInstance.activityMetric.get();
+    const barmode = templateInstance.barMode.get();
+
+    if (activityData) {
+      // Get translation strings
+      const activityTrendTitle = TAPi18n.__("residentActivityTrendChart-title");
+
+      // render chart
+      // Chart data consists of multiple traces
+      const data = _.map(activityData, function (activityCategoryData) {
+        // Create a trace for each activity type
+        return {
+          type: 'bar',
+          name: activityCategoryData.key,
+          // X values are activity dates
+          x: _.map(activityCategoryData.values, function (activityCategoryDay) {
+            return new Date(activityCategoryDay.key);
+          }),
+          y: _.map(activityCategoryData.values, function (activityCategoryDay) {
+            return activityCategoryDay.value[activityMetric];
+          }),
+        };
+      });
+
+      // Add plot layout configuration
+      const layout = {
+        title: activityTrendTitle,
+        xaxis: {
+          title: TAPi18n.__('reportPageActivitiesChart-xaxis-title'),
+        },
+        yaxis: {
+          title: TAPi18n.__(`reportPageActivitiesChart-yaxis-${activityMetric}`),
+        },
+        barmode,
+      };
+
+      // Get client locale
+      const locale = TAPi18n.getLanguage();
+
+      // Render plot
+      Plotly.newPlot('activityChart', data, layout, {locale});
+    }
+  })
+});
+
+Template.residentActivityTrendChart.events({
+  'change #activityMetric'(event, templateInstance) {
+    const activityMetric = $('input[name="activityMetric"]:checked').val();
+
+    templateInstance.activityMetric.set(activityMetric);
+  },
+  'change #barMode'(event, templateInstance) {
+    const barMode = $('input[name="barMode"]:checked').val();
+
+    templateInstance.barMode.set(barMode);
+  },
+  'change #timePeriod'(event, templateInstance) {
+    const timePeriod = $('input[name="timePeriod"]:checked').val();
+
+    templateInstance.timePeriod.set(timePeriod);
+  },
+});
