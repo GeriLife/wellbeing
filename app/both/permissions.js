4b3157c7ebd9f9b1669252a622bb7c9f58c94ea3
Brylie Christopher Oxley <brylie@amble.fi>
2019-03-17 01:29:09 +0200

    Add collection permissions

--- app/both/permissions.js
+++ app/both/permissions.js
@@ -53,3 +53,48 @@ Permissions.after.remove(function(userId, permission) {
     entityId: permission._id
   });
 });
+
+Permissions.allow({
+  insert: function() {
+    // Get current user ID
+    const currentUserId = Meteor.userId();
+
+    // Check if current user has Admin role
+    const currentUserIsAdmin = Roles.userIsInRole(currentUserId, ["admin"]);
+
+    // Only show edit column for users with Admin role
+    if (currentUserIsAdmin) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  update: function() {
+    // Get current user ID
+    const currentUserId = Meteor.userId();
+
+    // Check if current user has Admin role
+    const currentUserIsAdmin = Roles.userIsInRole(currentUserId, ["admin"]);
+
+    // Only show edit column for users with Admin role
+    if (currentUserIsAdmin) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  remove: function() {
+    // Get current user ID
+    const currentUserId = Meteor.userId();
+
+    // Check if current user has Admin role
+    const currentUserIsAdmin = Roles.userIsInRole(currentUserId, ["admin"]);
+
+    // Only show edit column for users with Admin role
+    if (currentUserIsAdmin) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+});
