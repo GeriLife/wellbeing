a17ffb561ab399b5cd69fa2dc1f6d22070724790
Brylie Christopher Oxley <brylie@amble.fi>
2019-03-17 01:53:28 +0200

    Lint

--- app/server/methods/permissions.js
+++ app/server/methods/permissions.js
@@ -2,7 +2,6 @@ Meteor.methods({
   addSingleUserPermissions(userId, groupIds) {
     if (groupIds.length > 0) {
       groupIds.forEach(groupId => {
-        console.log(userId, groupId);
         Permissions.insert({ userId, groupId });
       });
     }
