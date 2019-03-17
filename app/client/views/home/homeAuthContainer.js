466fc98a6cbd1e78e97d52bd08ec5d68e1f49f18
Brylie Christopher Oxley <brylie@amble.fi>
2019-03-17 23:37:13 +0200

    Ensure user is authorized to access home

--- app/client/views/home/homeAuthContainer.js
+++ app/client/views/home/homeAuthContainer.js
@@ -1,5 +1,24 @@
+Template.homeAuthContainer.onCreated(function() {
+  const templateInstance = this;
+  const router = Router.current();
+  const homeId = router.params.homeId;
+
+  templateInstance.userIsAuthorized = new ReactiveVar("spinner");
+  templateInstance.showSpinner = new ReactiveVar(true);
+
+  Meteor.call("currentUserCanAccessHome", homeId, function(
+    error,
+    userAuthorization
+  ) {
+    templateInstance.showSpinner.set(false);
+    templateInstance.userIsAuthorized.set(userAuthorization);
+  });
+});
+
 Template.homeAuthContainer.helpers({
   userIsAuthorized() {
-    return false;
+    const templateInstance = Template.instance();
+
+    return templateInstance.userIsAuthorized.get();
   }
 });
