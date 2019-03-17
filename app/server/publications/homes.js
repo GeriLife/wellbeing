75644ca1f49755e968ab653af631a205ed9d3e71
Brylie Christopher Oxley <brylie@amble.fi>
2019-03-17 15:13:25 +0200

    Lint

--- app/server/publications/homes.js
+++ app/server/publications/homes.js
@@ -1,14 +1,14 @@
-Meteor.publish('allHomes', function () {
+Meteor.publish("allHomes", function() {
   // All homes publication
   return Homes.find();
 });
 
-Meteor.publish('singleHome', function (homeId) {
+Meteor.publish("singleHome", function(homeId) {
   // Publish a single home
   return Homes.find(homeId);
 });
 
-Meteor.publish('homesBelongingToGroup', function (groupId) {
+Meteor.publish("homesBelongingToGroup", function(groupId) {
   // Publish homes belonging to group
-  return Homes.find({groupId: groupId});
+  return Homes.find({ groupId });
 });
