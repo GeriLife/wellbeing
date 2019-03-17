9b0d49a21eab24bee756b4b482e59024893002a3
Brylie Christopher Oxley <brylie@amble.fi>
2019-03-17 02:07:32 +0200

    Pre-populate select widget with existing permissions

--- app/client/views/settings/users/groups/edit.js
+++ app/client/views/settings/users/groups/edit.js
@@ -3,21 +3,36 @@ import "slim-select/dist/slimselect.min.css";
 
 Template.editUserGroups.onCreated(function() {
   const templateInstance = this;
+  const userId = templateInstance.data.user._id;
 
   templateInstance.subscribe("allGroups");
+  templateInstance.subscribe("userPermissions", userId);
 });
 
 Template.editUserGroups.onRendered(function() {
   const templateInstance = this;
+  const userId = templateInstance.data.user._id;
 
   const placeholder = "Select one or more groups";
 
-  // Render multi-select widget on 'select residents' field
-  templateInstance.groupSelect = new SlimSelect({
-    select: "[name=editUserGroups]",
-    closeOnSelect: false,
-    placeholder
-  });
+  if (templateInstance.subscriptionsReady()) {
+    // Render multi-select widget on 'select residents' field
+    templateInstance.groupSelect = new SlimSelect({
+      select: "[name=editUserGroups]",
+      closeOnSelect: false,
+      placeholder
+    });
+
+    // Check for existing permissions
+    const existingUserPermissions = Permissions.find({ userId }).fetch();
+
+    const existingUserGroupIds = _.map(existingUserPermissions, permission => {
+      return permission.groupId;
+    });
+
+    // Add existing groups to select widget, if any
+    templateInstance.groupSelect.set(existingUserGroupIds);
+  }
 });
 
 Template.editUserGroups.helpers({
