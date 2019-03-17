0a4f0e50724b3d3907df02b1b8b3a260e3ab9fa5
Brylie Christopher Oxley <brylie@amble.fi>
2019-03-17 15:02:42 +0200

    Add currentUserGroups publication

--- app/server/publications/groups.js
+++ app/server/publications/groups.js
@@ -1,9 +1,24 @@
-Meteor.publish('allGroups', function () {
+Meteor.publish("allGroups", function() {
   // Publish all groups
   return Groups.find();
 });
 
-Meteor.publish('singleGroup', function (groupId) {
+Meteor.publish("currentUserGroups", function() {
+  if (Meteor.user()) {
+    const userId = Meteor.user()._id;
+
+    // Check for existing permissions
+    const existingUserPermissions = Permissions.find({ userId }).fetch();
+
+    const existingUserPermissionGroupIds = existingUserPermissions.map(
+      permission => permission.groupId
+    );
+
+    return Groups.find({ _id: { $in: existingUserPermissionGroupIds } });
+  }
+});
+
+Meteor.publish("singleGroup", function(groupId) {
   // Publish only one group, specified as groupId
   return Groups.find(groupId);
 });
