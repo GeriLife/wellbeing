34119a35de9c7d3009d4a48a0dcb2e60d8c17366
Brylie Christopher Oxley <brylie@amble.fi>
2019-03-17 02:29:22 +0200

    Delete existing permission prior to inserting new set

--- app/server/methods/permissions.js
+++ app/server/methods/permissions.js
@@ -1,5 +1,9 @@
 Meteor.methods({
   addSingleUserPermissions(userId, groupIds) {
+    // Remove existing user permissions
+    Permissions.remove({ userId });
+
+    // add new permissions, if any group IDs provided
     if (groupIds.length > 0) {
       groupIds.forEach(groupId => {
         Permissions.insert({ userId, groupId });
