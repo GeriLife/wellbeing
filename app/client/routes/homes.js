8d528c5efed71ab2bff86590486a356bef480443
Brylie Christopher Oxley <brylie@amble.fi>
2019-03-17 22:18:09 +0200

    Cleanup/lint

--- app/client/routes/homes.js
+++ app/client/routes/homes.js
@@ -1,26 +1,32 @@
 /*
   Homes routes
 */
-Router.route('/homes', function () {
-  this.render('homes');
-}, {
-  name: 'homes'
-});
+Router.route(
+  "/homes",
+  function() {
+    this.render("homes");
+  },
+  {
+    name: "homes"
+  }
+);
 
-Router.route('/home/:homeId', function () {
-  this.render('home');
-}, {
-  name: 'home'
-});
+Router.route(
+  "/home/:homeId",
+  function() {
+    this.render("home");
+  },
+  {
+    name: "home"
+  }
+);
 
-Router.route('/home/:homeId/report', function () {
-  this.render('homeReport');
-}, {
-  name: 'homeReport'
-});
-
-Router.route('/latest-activities', function () {
-  this.render('latestActivitiesByType');
-}, {
-  name: 'latestActivitiesByType'
-});
+Router.route(
+  "/home/:homeId/report",
+  function() {
+    this.render("homeReport");
+  },
+  {
+    name: "homeReport"
+  }
+);
