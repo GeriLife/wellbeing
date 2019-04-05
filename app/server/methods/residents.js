184ad65ec46d2d55dfcb7308d842f5e1d793076e
Brylie Christopher Oxley <brylie@amble.fi>
2019-04-06 00:38:50 +0300

    Create "get user visible active resident IDs" helper

--- app/server/methods/residents.js
+++ app/server/methods/residents.js
@@ -1,22 +1,24 @@
-import moment from 'moment';
+import moment from "moment";
 
 Meteor.methods({
-  getAllResidentIds () {
+  getAllResidentIds() {
     // TODO: determine how to secure this method to prevent client abuse
 
     // Get all residents
     const residents = Residents.find().fetch();
 
     // Create an array of resident IDs
-    const residentIds = _.map(residents, function (resident) {
+    const residentIds = _.map(residents, function(resident) {
       return resident._id;
     });
 
     return residentIds;
   },
-  getResidentActivitiesByType ({ residentId, activityTypeId, period }) {
+  getResidentActivitiesByType({ residentId, activityTypeId, period }) {
     // Date period ago
-    const activityPeriodStart = moment().subtract(period, "days").toDate();
+    const activityPeriodStart = moment()
+      .subtract(period, "days")
+      .toDate();
 
     // Date today
     const now = new Date();
@@ -24,19 +26,25 @@ Meteor.methods({
     // Get all activities of a specific type involving resident
     // make sure activities are in the past (i.e. not planned)
     //  sort in reverse order by activity date
-    const activities = Activities.find({
-      'residentIds': residentId,
-      'activityTypeId': activityTypeId,
-      activityDate: {$gte: activityPeriodStart, $lte: now}},
-      {sort : {activityDate:  -1}
-    }).fetch();
+    const activities = Activities.find(
+      {
+        residentIds: residentId,
+        activityTypeId: activityTypeId,
+        activityDate: { $gte: activityPeriodStart, $lte: now }
+      },
+      { sort: { activityDate: -1 } }
+    ).fetch();
 
     if (activities) {
       return activities;
     }
   },
-  getCountOfResidentActivitiesByType ({ residentId, activityTypeId, period }) {
-    const activities = Meteor.call('getResidentActivitiesByType', { residentId, activityTypeId, period });
+  getCountOfResidentActivitiesByType({ residentId, activityTypeId, period }) {
+    const activities = Meteor.call("getResidentActivitiesByType", {
+      residentId,
+      activityTypeId,
+      period
+    });
 
     // Placeholder for sum of activities
     let activityCount = 0;
@@ -48,29 +56,31 @@ Meteor.methods({
 
     return activityCount;
   },
-  getAllResidentSelectOptions () {
+  getAllResidentSelectOptions() {
     /*
     return an array of resident objects in the form of:
        label: firstName lastInitial
        value: residentId
    */
 
-   // Get all residents
-   const residents = Residents.find().fetch();
+    // Get all residents
+    const residents = Residents.find().fetch();
 
-   // Create array of resident select options
-   const residentSelectOptions = _.map(residents, (resident) => {
-     return {
-       label: `${resident.firstName} ${resident.lastInitial}`,
-       value: resident._id,
-     }
-   });
+    // Create array of resident select options
+    const residentSelectOptions = _.map(residents, resident => {
+      return {
+        label: `${resident.firstName} ${resident.lastInitial}`,
+        value: resident._id
+      };
+    });
 
-   return residentSelectOptions;
+    return residentSelectOptions;
   },
-  getMinutesOfResidentActivitiesByType ({ residentId, activityTypeId, period }) {
+  getMinutesOfResidentActivitiesByType({ residentId, activityTypeId, period }) {
     // Date period ago
-    const activityPeriodStart = moment().subtract(period, "days").toDate();
+    const activityPeriodStart = moment()
+      .subtract(period, "days")
+      .toDate();
 
     // Date today
     const now = new Date();
@@ -79,22 +89,25 @@ Meteor.methods({
       {
         residentIds: residentId,
         activityTypeId: activityTypeId,
-        activityDate: {$gte: activityPeriodStart, $lte: now}
-        },
+        activityDate: { $gte: activityPeriodStart, $lte: now }
+      },
       {
-        sort : { activityDate:  -1 }
-      }).fetch();
+        sort: { activityDate: -1 }
+      }
+    ).fetch();
 
     // Sum of duration for particular resident & activity
-    return residentActivities.reduce((prevResult, document) => prevResult + document.duration, 0)
+    return residentActivities.reduce(
+      (prevResult, document) => prevResult + document.duration,
+      0
+    );
   },
-  residentNamesGroupedtByHomes(){
-
+  residentNamesGroupedtByHomes() {
     // Get list of homes, sorted alphabetically
     const homes = Homes.find({}, { sort: { name: 1 } }).fetch();
 
     // Create an array residents grouped by home
-    const residentsSelectOptions = _.map(homes, function (home) {
+    const residentsSelectOptions = _.map(homes, function(home) {
       // Get home ID
       const homeId = home._id;
 
@@ -134,5 +147,19 @@ Meteor.methods({
     });
 
     return residentsSelectOptions;
+    return residentActivities.reduce(
+      (prevResult, document) => prevResult + document.duration,
+      0
+    );
+  },
+  getUserVisibleActiveResidentIds(userId) {
+    const visibleResidencyIds = Meteor.call(
+      "getUserVisibleActiveResidencyIds",
+      userId
+    );
+
+    return Residencies.find({
+      _id: { $in: visibleResidencyIds }
+    }).map(residency => residency.residentId);
   }
 });
