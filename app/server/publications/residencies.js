22595c37336f1e1b1567fd84518ee734021259cb
Brylie Christopher Oxley <brylie@amble.fi>
2019-04-05 23:42:28 +0300

    Add current user visible active residencies

--- app/server/publications/residencies.js
+++ app/server/publications/residencies.js
@@ -1,16 +1,35 @@
-Meteor.publish('singleResidency', function (residentId, homeId) {
+Meteor.publish("singleResidency", function(residentId, homeId) {
   // Publish a single residency
   return Residencies.find({ residentId, homeId });
 });
 
-Meteor.publish('allResidencies', function () {
+Meteor.publish("allResidencies", function() {
   // Publish all residencies
   return Residencies.find();
 });
 
-Meteor.publish('allCurrentResidencies', function () {
+Meteor.publish("allCurrentResidencies", function() {
   // Publish all current residencies
   return Residencies.find({
-    moveOut: {$exists: false},
+    moveOut: { $exists: false }
+  });
+});
+
+Meteor.publish("currentUserVisibleActiveResidencies", function() {
+  const userId = Meteor.userId();
+
+  const userVisibleHomeIds = Meteor.call("getUserVisibleHomeIds", userId);
+
+  // do not show departed residents
+  // residencies without moved out date
+  const notDeparted = {
+    moveOut: {
+      $exists: false
+    }
+  };
+
+  return Residencies.find({
+    homeId: { $in: userVisibleHomeIds },
+    ...notDeparted
   });
 });
