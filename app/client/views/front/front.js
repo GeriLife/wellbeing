fd04aac98b9b74dbd60e401d554b11626fd23c40
shailee mehta <mehtashailee21@gmail.com>
2019-04-08 22:05:26 +0530

    Converting front component to vue without localization

--- app/client/views/front/front.js
+++ app/client/views/front/front.js
@@ -1,17 +1,7 @@
-Template.front.events({
-  'click #add-activity' () {
-    // Show the add activity modal
-    Modal.show('activityFormModal');
-  },
-  'click #add-feeling' () {
-    // Show the feeling modal
-    Modal.show('newFeeling');
-  }
-});
-
-
 Template.front.helpers({
   props(){
-    return { val:"Hello to Vue from blaze!"}
+    return { 
+      Modal
+    }
   }
 })
