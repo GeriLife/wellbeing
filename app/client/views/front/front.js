7e5a75e4bf086b0c5edb1703561d744a90ed0e2d
shailee mehta <mehtashailee21@gmail.com>
2019-04-10 23:09:44 +0530

    adding localization to vue component

--- app/client/views/front/front.js
+++ app/client/views/front/front.js
@@ -1,7 +1,28 @@
 Template.front.helpers({
-  props(){
-    return { 
-      Modal
-    }
+  props() {
+    return {
+      Modal,
+      localizedText: {
+        "quickAddTitle": TAPi18n.__("front-quickAddPanel-title"),
+        "activityButton": TAPi18n.__(
+          "front-quickAddPanel-activityButton"
+        ),
+        "feelingButton": TAPi18n.__(
+          "front-quickAddPanel-feelingButton"
+        ),
+        "linkTitle": TAPi18n.__(
+          "front-quickNavigationPanel-title"
+        ),
+        "residentsButton": TAPi18n.__(
+          "front-quickNavigationPanel-residentsButton"
+        ),
+        "homesButton": TAPi18n.__(
+          "front-quickNavigationPanel-homesButton"
+        ),
+        "activitiesButton": TAPi18n.__(
+          "front-quickNavigationPanel-activitiesButton"
+        )
+      }
+    };
   }
-})
+});
