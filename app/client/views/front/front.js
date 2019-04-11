6862f1b028a668b1be03e156476ef7e614bf5865
shailee mehta <mehtashailee21@gmail.com>
2019-04-11 23:41:04 +0530

    WIP: minimizing code

--- app/client/views/front/front.js
+++ app/client/views/front/front.js
@@ -2,27 +2,7 @@ Template.front.helpers({
   props() {
     return {
       Modal,
-      localizedText: {
-        "quickAddTitle": TAPi18n.__("front-quickAddPanel-title"),
-        "activityButton": TAPi18n.__(
-          "front-quickAddPanel-activityButton"
-        ),
-        "feelingButton": TAPi18n.__(
-          "front-quickAddPanel-feelingButton"
-        ),
-        "linkTitle": TAPi18n.__(
-          "front-quickNavigationPanel-title"
-        ),
-        "residentsButton": TAPi18n.__(
-          "front-quickNavigationPanel-residentsButton"
-        ),
-        "homesButton": TAPi18n.__(
-          "front-quickNavigationPanel-homesButton"
-        ),
-        "activitiesButton": TAPi18n.__(
-          "front-quickNavigationPanel-activitiesButton"
-        )
-      }
+      localize:TAPi18n.__
     };
   }
 });
