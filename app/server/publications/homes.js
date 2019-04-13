8fc46d7639c51fa2616175e89e29c692e196edea
Brylie Christopher Oxley <brylie@amble.fi>
2019-04-14 01:00:36 +0300

    Check for admin

--- app/server/publications/homes.js
+++ app/server/publications/homes.js
@@ -14,12 +14,19 @@ Meteor.publish("homesBelongingToGroup", function(groupId) {
 });
 
 Meteor.publish("currentUserVisibleHomes", function() {
+  const selector = {};
+
   const userId = Meteor.userId();
 
-  // Group IDs can be obtained through Permissions
-  const userGroups = Meteor.call("getSingleUserGroupIds", userId);
+  const userIsAdmin = Roles.userIsInRole(userId, "admin");
 
-  return Homes.find({
-    groupId: { $in: userGroups }
-  });
+  // non-admin should see only homes belonging to assigned group(s)
+  if (!userIsAdmin) {
+    // Group IDs can be obtained through Permissions
+    const userGroups = Meteor.call("getSingleUserGroupIds", userId);
+
+    selector.groupId = { $in: userGroups };
+  }
+
+  return Homes.find(selector);
 });
