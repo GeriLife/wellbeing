a6e0dc5233b51dd94b42a064e6bd2e3d795ab0e9
Brylie Christopher Oxley <brylie@amble.fi>
2019-04-15 02:08:14 +0300

    Lint

--- app/client/views/activities/form/autoform.js
+++ app/client/views/activities/form/autoform.js
@@ -1,45 +1,46 @@
-import SlimSelect from 'slim-select';
-import 'slim-select/dist/slimselect.min.css';
+import SlimSelect from "slim-select";
+import "slim-select/dist/slimselect.min.css";
 import flatpickr from "flatpickr";
-import 'flatpickr/dist/flatpickr.min.css';
-import moment from 'moment';
+import "flatpickr/dist/flatpickr.min.css";
+import moment from "moment";
 import { Finnish } from "flatpickr/dist/l10n/fi.js";
 
-AutoForm.addHooks(['activityForm'], {
-  'onSuccess': function (formType) {
+AutoForm.addHooks(["activityForm"], {
+  onSuccess: function(formType) {
     // Hide the modal dialogue
-    Modal.hide('activityForm');
+    Modal.hide("activityForm");
 
     // placeholder for success message text
     let successMessage;
 
     // Get relevant success message for form type
-    if (formType === 'update') {
+    if (formType === "update") {
       successMessage = TAPi18n.__("activityForm-update-success");
     } else {
       successMessage = TAPi18n.__("activityForm-add-success");
     }
 
-    const successMessageWithIcon = '<i class="fa fa-check"></i> ' + successMessage
+    const successMessageWithIcon =
+      '<i class="fa fa-check"></i> ' + successMessage;
 
     // Alert user that activity was added
     FlashMessages.sendSuccess(successMessageWithIcon);
   }
 });
 
-Template.autoForm.onRendered(function () {
+Template.autoForm.onRendered(function() {
   const instance = this;
 
   // Make sure the new activity form is being rendered
   if (instance.data.id === "activityForm") {
     // Get localized placeholder text
-    const placeholder = TAPi18n.__('activityForm-residentSelect-placeholder');
+    const placeholder = TAPi18n.__("activityForm-residentSelect-placeholder");
 
     // Render multi-select widget on 'select residents' field
     new SlimSelect({
-      select: '[name=residentIds]',
+      select: "[name=residentIds]",
       closeOnSelect: false,
-      placeholder,
+      placeholder
     });
 
     // Get user locale
@@ -49,7 +50,7 @@ Template.autoForm.onRendered(function () {
     let locale;
 
     // Localize calendar widget based on user locale
-    if (userLocale === 'fi') {
+    if (userLocale === "fi") {
       locale = Finnish;
     } else {
       locale = flatpickr.l10ns.default;
@@ -58,9 +59,12 @@ Template.autoForm.onRendered(function () {
     // Render cross-platform date picker on date field
     flatpickr("#activityDate", {
       // Minimum day should be within previous seven days, including today
-      minDate: moment().subtract(6, 'days').startOf('day').toDate(),
+      minDate: moment()
+        .subtract(6, "days")
+        .startOf("day")
+        .toDate(),
       maxDate: "today",
-      locale,
+      locale
     });
   }
 });
