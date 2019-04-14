cf97649e4960f1431cd274aa7de5a38afff592f1
Brylie Christopher Oxley <brylie@amble.fi>
2019-04-15 01:14:58 +0300

    Use Slim Select

--- app/client/views/feelings/new/autoform.js
+++ app/client/views/feelings/new/autoform.js
@@ -1,12 +1,32 @@
-AutoForm.addHooks(['newFeelingForm'], {
-  'onSuccess': function () {
+import SlimSelect from "slim-select";
+import "slim-select/dist/slimselect.min.css";
+
+AutoForm.addHooks(["newFeelingForm"], {
+  onSuccess: function() {
     // Hide the modal dialogue
-    Modal.hide('newFeeling');
+    Modal.hide("newFeeling");
 
     // Get success message translation
     const successMessage = TAPi18n.__("newFeeling-success");
 
     // Alert user that feeling was recorded
     FlashMessages.sendSuccess('<i class="fa fa-check"></i> ' + successMessage);
+  }
+});
+
+Template.autoForm.onRendered(function() {
+  const templateInstance = this;
+
+  // Make sure the new activity form is being rendered
+  if (templateInstance.data.id === "newFeelingForm") {
+    // Get localized placeholder text
+    const placeholder = TAPi18n.__("activityForm-residentSelect-placeholder");
+
+    // Render multi-select widget on 'select resident' field
+    new SlimSelect({
+      select: "[name=residentId]",
+      closeOnSelect: true,
+      placeholder
+    });
   }
 });
