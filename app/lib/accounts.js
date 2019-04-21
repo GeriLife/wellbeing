f3e8202bd657b1b359e5fffc37bbbc6f0ccf709b
shailee mehta <mehtashailee21@gmail.com>
2019-04-21 20:41:38 +0530

    setting passwordEnrollTokenExpirationInDays flag to keep the token valid for 60days

--- app/lib/accounts.js
+++ app/lib/accounts.js
@@ -4,10 +4,12 @@ AccountsTemplates.configure({
   enablePasswordChange: true,
   showForgotPasswordLink: true
 });
-
+Accounts.config({
+  passwordEnrollTokenExpirationInDays: 60
+});
 // Configure AccountsTemplates routes
-AccountsTemplates.configureRoute('changePwd');
-AccountsTemplates.configureRoute('enrollAccount');
-AccountsTemplates.configureRoute('forgotPwd');
-AccountsTemplates.configureRoute('resetPwd');
-AccountsTemplates.configureRoute('signIn');
+AccountsTemplates.configureRoute("changePwd");
+AccountsTemplates.configureRoute("enrollAccount");
+AccountsTemplates.configureRoute("forgotPwd");
+AccountsTemplates.configureRoute("resetPwd");
+AccountsTemplates.configureRoute("signIn");
