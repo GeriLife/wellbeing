2dde791de75f68bde17adf3dd2b1b773b5c7065c
shailee mehta <mehtashailee21@gmail.com>
2019-04-29 23:57:35 +0530

    adding plotly chart and removing Dimple

--- app/client/views/resident/activities/types/types.js
+++ app/client/views/resident/activities/types/types.js
@@ -1,8 +1,8 @@
-Template.residentActivityTypesChart.onRendered(function () {
+Template.residentActivityTypesChart.onRendered(function() {
   // Get reference to template instance
   var instance = Template.instance();
 
-  instance.autorun(function () {
+  instance.autorun(function() {
     // Empty the activity types chart, in case of data change
     $("#activityTypesChart").empty();
 
@@ -11,7 +11,7 @@ Template.residentActivityTypesChart.onRendered(function () {
 
     // Add 'type' field to each activity,
     // Containing activity type name
-    activities = _.map(activities, function (activity) {
+    activities = _.map(activities, function(activity) {
       // Get activity type name via collection helper
       activity.type = activity.activityType();
 
@@ -19,46 +19,52 @@ Template.residentActivityTypesChart.onRendered(function () {
     });
 
     // Group activities by activity type
-    const activityTypeCounts = d3.nest()
-      .key(function (activity) {
+    const activityTypeCounts = d3
+      .nest()
+      .key(function(activity) {
         return activity.type;
       })
-      .rollup(function (activityType) { return activityType.length })
+      .rollup(function(activityType) {
+        return activityType.length;
+      })
       .entries(activities);
 
-      // Get reference to chart container
-      const svg = dimple.newSvg("#activityTypesChart", "100%", "100%");
+    if (activityTypeCounts && activityTypeCounts.length > 0) {
+      const data = [
+        {
+          type: "bar",
+          y: _.map(activityTypeCounts, r => r.key.substr(0, 6)),
+          x: _.map(activityTypeCounts, r => r.values),
+          text: _.map(
+            activityTypeCounts,
+            r => `key:${r.key} \n value:${r.values}`
+          ),
+          marker: {
+            color: "#9ac0db"
+          },
+          width: 0.5,
+          orientation: "h"
+        }
+      ];
 
-      // Initialize the activity type chart
-      const activityTypesChart = new dimple.chart(svg, activityTypeCounts);
+      // Add plot layout configuration
+      const layout = {
+        xaxis: {
+          title: TAPi18n.__("residentActivityTypesChart-xAxis-title")
+        },
+        yaxis: {
+          title: TAPi18n.__("residentActivityTypesChart-yAxis-title")
+        },
+        width: 600,
+        height: 350
+        // bargap: 0.8
+      };
 
-      // Set chart boundaries based on parent container size
-      activityTypesChart.setBounds("30%", "5%", "65%", "60%");
+      // Get client locale
+      const locale = TAPi18n.getLanguage();
 
-      // Add activity types to x axis
-      const xAxis = activityTypesChart.addMeasureAxis("x", "values");
-
-      // Set the y axis title
-      const xAxisTitle = TAPi18n.__("residentActivityTypesChart-xAxis-title");
-      xAxis.title = xAxisTitle;
-
-      // Disable grid lines
-      xAxis.showGridlines = false;
-
-      // Add activity type counts to y axis
-      const yAxis = activityTypesChart.addCategoryAxis("y", "key");
-
-      // Set the y axis title
-      const yAxisTitle = TAPi18n.__("residentActivityTypesChart-yAxis-title");
-      yAxis.title = yAxisTitle;
-
-      // Reduce the x axis font size, so labels do not get truncated
-      yAxis.fontSize = "8px";
-
-      // Add bar plot
-      activityTypesChart.addSeries(null, dimple.plot.bar);
-
-      // Render chart
-      activityTypesChart.draw();
+      // Render plot
+      Plotly.newPlot("activityTypesChart", data, layout, { locale });
+    }
   });
 });
