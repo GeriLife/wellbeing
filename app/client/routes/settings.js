44f70031b9496cd18545ba49284d9bd82434c4c2
Brylie Christopher Oxley <brylie@gnumedia.org>
2019-05-01 15:19:24 +0300

    Lint

--- app/client/routes/settings.js
+++ app/client/routes/settings.js
@@ -2,69 +2,97 @@
   Settings routes
 */
 
-Router.route('/settings', function () {
-  this.layout('settingsLayout');
-  this.render('settings');
-}, {
-  name: 'settings',
-});
+Router.route(
+  "/settings",
+  function() {
+    this.layout("settingsLayout");
+    this.render("settings");
+  },
+  {
+    name: "settings"
+  }
+);
 
 /*
 Activity Types
 */
-Router.route('/settings/activity-types', function () {
-  this.layout('settingsLayout');
-  this.render('activityTypesSettings');
-}, {
-  name: 'activityTypesSettings'
-});
+Router.route(
+  "/settings/activity-types",
+  function() {
+    this.layout("settingsLayout");
+    this.render("activityTypesSettings");
+  },
+  {
+    name: "activityTypesSettings"
+  }
+);
 
 /*
 Data
 */
-Router.route('/settings/data', function () {
-  this.layout('settingsLayout');
-  this.render('dataSettings');
-}, {
-  name: 'dataSettings'
-});
+Router.route(
+  "/settings/data",
+  function() {
+    this.layout("settingsLayout");
+    this.render("dataSettings");
+  },
+  {
+    name: "dataSettings"
+  }
+);
 
 /*
 Date/time
 */
-Router.route('/settings/datetime', function () {
-  this.layout('settingsLayout');
-  this.render('dateTimeSettings');
-}, {
-  name: 'dateTimeSettings',
-});
+Router.route(
+  "/settings/datetime",
+  function() {
+    this.layout("settingsLayout");
+    this.render("dateTimeSettings");
+  },
+  {
+    name: "dateTimeSettings"
+  }
+);
 
 /*
 Roles
 */
-Router.route('/settings/roles', function () {
-  this.layout('settingsLayout');
-  this.render('rolesSettings');
-}, {
-  name: 'rolesSettings'
-});
+Router.route(
+  "/settings/roles",
+  function() {
+    this.layout("settingsLayout");
+    this.render("rolesSettings");
+  },
+  {
+    name: "rolesSettings"
+  }
+);
 
 /*
 User event log
 */
-Router.route('/settings/event-log', function () {
-  this.layout('settingsLayout');
-  this.render('userEventLog');
-}, {
-  name: 'userEventLog'
-});
+Router.route(
+  "/settings/event-log",
+  function() {
+    this.layout("settingsLayout");
+    this.render("userEventLog");
+  },
+  {
+    name: "userEventLog"
+  }
+);
 
 /*
 Users
 */
-Router.route('/settings/users', function () {
-  this.layout('settingsLayout');
-  this.render('usersSettings');
-}, {
-  name: 'usersSettings'
-});
+Router.route(
+  "/settings/users",
+  function() {
+    this.layout("settingsLayout");
+    this.render("usersSettings");
+  },
+  {
+    name: "usersSettings"
+  }
+);
