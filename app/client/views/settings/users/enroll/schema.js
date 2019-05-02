cb1643de1f3bd0e39cfc9c453110a5227d9be5bf
shailee mehta <mehtashailee21@gmail.com>
2019-05-02 23:05:46 +0530

    adding groups when inviting

--- app/client/views/settings/users/enroll/schema.js
+++ app/client/views/settings/users/enroll/schema.js
@@ -1,24 +1,47 @@
-import SimpleSchema from 'simpl-schema';
+import SimpleSchema from "simpl-schema";
+SimpleSchema.extendOptions(['autoform']);
 
-const usersEnrollSchema = new SimpleSchema({
-  subject: {
-    type: String,
-    // suggested maximum length of email subject
-    // https://stackoverflow.com/a/1592310/1191545
-    max: 78
-  },
-  message: {
-    type: String,
-    optional: false
-  },
-  emailAddresses: {
-    type: Array,
-    optional: false
-  },
-  'emailAddresses.$': {
-    type: String,
-    regEx: SimpleSchema.RegEx.Email,
-  }
-});
+const usersEnrollSchema = function(groups) {
+  return new SimpleSchema({
+    subject: {
+      type: String,
+      // suggested maximum length of email subject
+      // https://stackoverflow.com/a/1592310/1191545
+      max: 78
+    },
+    message: {
+      type: String,
+      optional: false
+    },
+    emailAddresses: {
+      type: Array,
+      optional: false
+    },
+    "emailAddresses.$": {
+      type: String,
+      regEx: SimpleSchema.RegEx.Email
+    },
+    groups: {
+      type: Array,
+      optional: true,
+
+      label: "Add group",
+      
+      autoform: {
+        options: function() {
+          return groups.map(r => {
+            return {
+              label: r.name,
+              value: r._id
+            };
+          });
+        }
+      }
+    },
+    'groups.$':{
+      type:String
+    }
+  });
+};
 
 export default usersEnrollSchema;
