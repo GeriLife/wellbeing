812db327cb604676d67db30683a72413a8c2adee
shailee mehta <mehtashailee21@gmail.com>
2019-05-03 23:34:33 +0530

    removing select2

--- app/client/layouts/main/main.js
+++ app/client/layouts/main/main.js
@@ -1,5 +1,2 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'select2';
-import 'select2/dist/css/select2.css';
-import 'select2-bootstrap-theme/dist/select2-bootstrap.css';
