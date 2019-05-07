68289236059c7e2a539b6822d1857a5b54231b90
Brylie Christopher Oxley <brylie@amble.fi>
2019-05-07 21:05:05 +0300

    Get from email from environment

--- app/lib/accounts.js
+++ app/lib/accounts.js
@@ -4,12 +4,18 @@ AccountsTemplates.configure({
   enablePasswordChange: true,
   showForgotPasswordLink: true
 });
+
 Accounts.config({
   passwordEnrollTokenExpirationInDays: 60
 });
+
 // Configure AccountsTemplates routes
 AccountsTemplates.configureRoute("changePwd");
 AccountsTemplates.configureRoute("enrollAccount");
 AccountsTemplates.configureRoute("forgotPwd");
 AccountsTemplates.configureRoute("resetPwd");
 AccountsTemplates.configureRoute("signIn");
+
+Meteor.startup(function() {
+  Accounts.emailTemplates.from = process.env.EMAIL_FROM;
+});
