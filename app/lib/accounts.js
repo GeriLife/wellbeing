4841e5fa352db3e63c56234145cdc3fcd6c28e3a
shailee mehta <mehtashailee21@gmail.com>
2019-05-14 21:56:37 +0530

    Adding validation to email address with TLD

--- app/lib/accounts.js
+++ app/lib/accounts.js
@@ -16,6 +16,25 @@ AccountsTemplates.configureRoute("forgotPwd");
 AccountsTemplates.configureRoute("resetPwd");
 AccountsTemplates.configureRoute("signIn");
 
-Meteor.startup(function() {
+Meteor.startup(function () {
+  Accounts.validateNewUser(function (user) {
+    if (!user) return false
+    if (!user.emails || (!!user.emails && user.emails.length === 0)) return false
+    const emailValidationRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    /* Iterates through all emails and checks if all emails are valid */
+    const areAllEmailsValid = user.emails.every(emailObject => {
+      const emailAddress = emailObject.address;
+      if (!emailAddress) return false
+      return emailValidationRegex.test(emailAddress);
+    })
+    if (!areAllEmailsValid)
+      // Must throw meteor error to show custom validation message
+      throw new Meteor.Error(500, "Email address is not valid!");
+    
+    // return true in all other cases
+    return true
+
+  });
   Accounts.emailTemplates.from = process.env.FROM_EMAIL;
+  
 });
