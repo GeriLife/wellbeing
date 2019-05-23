e1be34cfa7308167be32341ba6bc08b63197562a
shailee mehta <mehtashailee21@gmail.com>
2019-05-23 23:30:54 +0530

    show current managers

--- app/client/views/homes/assignManger/assignManager.js
+++ app/client/views/homes/assignManger/assignManager.js
@@ -3,6 +3,15 @@ import assignManagerSchema from "./schema";
 Template.assignManager.onCreated(function() {
   const templateInstance = this;
   templateInstance.subscribe("allUsers");
+  templateInstance.currentManagers = new ReactiveVar(null);
+  Meteor.call("getCurrentManagers", templateInstance.data.groupId, function(
+    err,
+    userEmails
+  ) {
+    if (!err && userEmails && userEmails.length > 0) {
+      templateInstance.currentManagers.set(userEmails);
+    }
+  });
 });
 
 Template.assignManager.helpers({
@@ -11,5 +20,10 @@ Template.assignManager.helpers({
   },
   buttonContent() {
     return TAPi18n.__("assignManager-form-submitButton-text");
+  },
+  currentManagers() {
+    const currentManagers = Template.instance().currentManagers.get();
+    if (!currentManagers) return currentManagers;
+    return currentManagers.join(", ")
   }
 });
