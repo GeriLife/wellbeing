ba6b7dffbaef76326a8b4c2495c0b5501f4237bd
shailee mehta <mehtashailee21@gmail.com>
2019-06-04 22:27:44 +0530

    adding flash messages to show form validations in add new residency modal

--- app/client/views/residencies/forms/add/autoform.js
+++ app/client/views/residencies/forms/add/autoform.js
@@ -1,20 +1,33 @@
 AutoForm.hooks({
   addNewResidentAndResidency: {
-    onSuccess (formType, success) {
+    onSuccess(formType, success) {
       // check if submission was successful
       if (success) {
         // Hide modal dialogue
-        Modal.hide('addResidencyModal');
+        Modal.hide("addResidencyModal");
       }
     },
+    onError
   },
   addResidencyForExistingResident: {
-    onSuccess (formType, success) {
+    onSuccess(formType, success) {
       // check if submission was successful
       if (success) {
         // Hide modal dialogue
-        Modal.hide('addResidencyModal');
+        Modal.hide("addResidencyModal");
       }
     },
+    onError
   }
 });
+
+function onError(formType, error) {
+  if (error.error) {
+    FlashMessages.sendError(`${error.error}: ${error.reason}`, {
+      autoHide: false
+    });
+  } else if (error.message) {
+    /* If a form input is invalid flash message is displayed*/
+    FlashMessages.sendError(error.message, { autoHide: false });
+  }
+}
