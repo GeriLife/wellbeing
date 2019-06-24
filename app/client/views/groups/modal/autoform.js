c3e62172c5e6bce16ee86299da7ab36461a89544
shailee mehta <mehtashailee21@gmail.com>
2019-06-24 22:21:48 +0530

    Adding error messages when input is empty following modals: 1. add/edit group name 2. add/edit home name

--- app/client/views/groups/modal/autoform.js
+++ app/client/views/groups/modal/autoform.js
@@ -1,6 +1,11 @@
 AutoForm.addHooks(['groupForm'], {
-  'onSuccess': function () {
+  onSuccess: function() {
     // Hide the modal dialogue
     Modal.hide('newGroup');
-  }
+  },
+  onError: function(formType, error) {
+    FlashMessages.sendError('<i class="fa fa-warning"></i> ' + error.message, {
+      autoHide: true,
+    });
+  },
 });
