bab8b691c1d7a250b2ed6d55055031f4ab4d5fcf
shailee mehta <mehtashailee21@gmail.com>
2019-07-01 23:03:56 +0530

    Not setting deactivateOn field by default Set only when chose by user

--- app/server/crons/deactivateUsers.js
+++ app/server/crons/deactivateUsers.js
@@ -4,22 +4,20 @@ new CronJob(
   '0 0 12 * * *',
   Meteor.bindEnvironment(function(err, data) {
     /* Get all users for whom the deactivation date is set */
-    const users = Meteor.users
-      .find({
+    Meteor.users.update(
+      {
         $and: [
           { deactivateOn: { $lte: new Date() } },
-          { deactivateOn: { $gt: new Date(0) } },
+          { deactivateOn: { $exists: true } },
         ],
-      })
-      .fetch();
-
-    /* For all retrieved users */
-    _.each(users, function(user) {
-      /* Make the user Inactive */
-      Meteor.users.update(user, {
+      },
+      {
         $set: { isActive: false },
-      });
-    });
+      },
+      {
+        multi: true,
+      }
+    );
   }),
   null,
   true
