15a4964d5072f28daccd62c83764bb58290f34e9
Shailee Mehta <mehtashailee21@gmail.com>
2019-08-14 21:27:34 +0200

    WIP activity test cases

---
+++ app/imports/activities/mockData.js
@@ -0,0 +1,14 @@
+export const mockInsertData = {
+  residentIds: ['12333'],
+  activityTypeId: '1223',
+  activityDate: new Date(),
+  duration: 5,
+  facilitatorRoleId: '1233456',
+};
+
+export const insertWithoutduration = {
+  residentIds: ['12333'],
+  activityTypeId: '1223',
+  activityDate: new Date(),
+  facilitatorRoleId: '1233456',
+};
