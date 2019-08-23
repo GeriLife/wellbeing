32153a0e4a78df189a2d5e3b4598965fe3901519
Shailee Mehta <mehtashailee21@gmail.com>
2019-08-24 00:14:26 +0200

    making text field of type password

--- app/client/views/settings/users/form/newUserSchema.js
+++ app/client/views/settings/users/form/newUserSchema.js
@@ -4,12 +4,16 @@ import {
   emailSchema,
   isAdminSchema,
 } from './commonSchemas';
+SimpleSchema.extendOptions(['autoform']);
 
 export const NewUserSchema = new SimpleSchema({
   email: emailSchema,
   password: {
     type: String,
     min: 5,
+    autoform: {
+      type: 'password',
+    },
   },
   isAdmin: isAdminSchema,
 
