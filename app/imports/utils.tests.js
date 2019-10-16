1837ca556447fdfcdad5654cc5e47de96b75e6bd
Shailee Mehta <mehtashailee21@gmail.com>
2019-10-17 01:34:11 +0200

    sonar issues

--- app/imports/utils.tests.js
+++ app/imports/utils.tests.js
@@ -21,17 +21,14 @@ export const setupDbUsser = function(user, cb) {
 };
 
 export const createManager = function(groupId, user, cb) {
-  createTestUser(user, function(err, user) {
+  createTestUser(user, function(err, insertedUser) {
     if (err) {
-      cb(err);
-    } else {
-      let userId;
-      if (typeof user === 'string') {
-        userId = user;
-      } else {
-        userId = user._id;
-      }
-      Meteor.call('assignManager', { groupId, users: [userId] }, cb);
+      return cb(err);
     }
+    let userId = insertedUser;
+    if (typeof insertedUser !== 'string') {
+      userId = insertedUser._id;
+    }
+    Meteor.call('assignManager', { groupId, users: [userId] }, cb);
   });
 };
