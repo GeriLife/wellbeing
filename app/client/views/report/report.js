08ae0a019892268c30bc68c290d6c3f785a7845f
shailee mehta <mehtashailee21@gmail.com>
2019-12-18 23:18:58 +0530

    [500] Added a cron job that runs every day to aggregate the report Added a new method to fetch the reports on the All reports page

--- app/client/views/report/report.js
+++ app/client/views/report/report.js
@@ -1,4 +1,5 @@
 import _ from "lodash";
+import moment from 'moment';
 
 Template.report.onCreated(function () {
   const templateInstance = this;
@@ -7,6 +8,7 @@ Template.report.onCreated(function () {
   templateInstance.activityMetric = new ReactiveVar();
   templateInstance.timePeriod = new ReactiveVar();
   templateInstance.barMode = new ReactiveVar();
+  templateInstance.lastUpdated = new ReactiveVar();
 });
 
 Template.report.onRendered(function () {
@@ -25,9 +27,11 @@ Template.report.onRendered(function () {
     const timePeriod = templateInstance.timePeriod.get();
 
     // call method to retrieve aggregated activity data
-    Meteor.call('getAggregatedActivities', timePeriod, function (error, activityData) {
+    Meteor.call('getActivitiesAggregateReport', timePeriod, function (error, { activityData, lastUpdated }) {
+      
       // set activity data in the reactive variable
       templateInstance.activityData.set(activityData);
+      templateInstance.lastUpdated.set(lastUpdated);
     });
   });
 
@@ -93,5 +97,12 @@ Template.report.events({
     const timePeriod = $('input[name="timePeriod"]:checked').val();
 
     templateInstance.timePeriod.set(timePeriod);
+  },
+});
+
+Template.report.helpers({
+  lastUpdated() {
+    const instance = Template.instance();
+    return moment(instance.lastUpdated.get()).format('DD-MM-YYYY');
   },
 });
