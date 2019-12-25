229e3a15efb7e475182f940303f90a81b77c11cf
shailee mehta <mehtashailee21@gmail.com>
2019-12-25 20:09:31 +0530

    created a common method to caclulate all aggregates
    Added second chart on the all reports page and added related localisation texts
    
    Updated cron job to also aggregate the facilitator roles data

--- app/client/views/report/report.js
+++ app/client/views/report/report.js
@@ -1,21 +1,25 @@
-import _ from "lodash";
+import _ from 'lodash';
 import moment from 'moment';
 
-Template.report.onCreated(function () {
+Template.report.onCreated(function() {
   const templateInstance = this;
 
-  templateInstance.activityData = new ReactiveVar();
+  templateInstance.activityDataByType = new ReactiveVar();
+  templateInstance.activityDataByRole = new ReactiveVar();
   templateInstance.activityMetric = new ReactiveVar();
   templateInstance.timePeriod = new ReactiveVar();
   templateInstance.barMode = new ReactiveVar('stack');
-  templateInstance.lastUpdated = new ReactiveVar();
+  templateInstance.lastUpdatedActivityType = new ReactiveVar();
+  templateInstance.lastUpdatedActivityRole = new ReactiveVar();
 });
 
-Template.report.onRendered(function () {
+Template.report.onRendered(function() {
   const templateInstance = this;
 
   // Set initial activity metric from template
-  const activityMetric = $('input[name="activityMetric"]:checked').val();
+  const activityMetric = $(
+    'input[name="activityMetric"]:checked'
+  ).val();
   templateInstance.activityMetric.set(activityMetric);
 
   // Set initial time period from template
@@ -23,68 +27,55 @@ Template.report.onRendered(function () {
   templateInstance.timePeriod.set(timePeriod);
 
   // fetch chart data in reactive context
-  templateInstance.autorun(function () {
+  templateInstance.autorun(function() {
     const timePeriod = templateInstance.timePeriod.get();
 
     // call method to retrieve aggregated activity data
-    Meteor.call('getActivitiesAggregateReport', timePeriod, function (error, { activityData, lastUpdated }) {
-      
-      // set activity data in the reactive variable
-      templateInstance.activityData.set(activityData);
-      templateInstance.lastUpdated.set(lastUpdated);
-    });
+    Meteor.call(
+      'getActivitiesAggregateReport',
+      timePeriod,
+      'type',
+      function(error, { activityData, lastUpdated }) {
+        // set activity data in the reactive variable
+        templateInstance.activityDataByType.set(activityData);
+        templateInstance.lastUpdatedActivityType.set(lastUpdated);
+      }
+    );
+
+    Meteor.call(
+      'getActivitiesAggregateReport',
+      timePeriod,
+      'facilitator',
+      function(error, { activityData, lastUpdated }) {
+        // set activity data in the reactive variable
+        templateInstance.activityDataByRole.set(activityData);
+        templateInstance.lastUpdatedActivityRole.set(lastUpdated);
+      }
+    );
   });
 
   // Render chart in reactive context
-  templateInstance.autorun(function () {
-    const activityData = templateInstance.activityData.get();
-    const activityMetric = templateInstance.activityMetric.get();
-    const barmode = templateInstance.barMode.get();
-
-    // Make sure we have some activity data
-    if (activityData) {
-      // Chart data consists of multiple traces
-      const data = _.map(activityData, function (activityCategoryData) {
-        // Create a trace for each activity type
-        return  {
-          type: 'bar',
-          name: activityCategoryData.key,
-          // X values are activity dates
-          x: _.map(activityCategoryData.values, function(activityCategoryDay) {
-            return new Date(activityCategoryDay.key);
-          }),
-          // Y values are (currently) activity minutes
-          // TODO: add page element to toggle between activity counts and minutes
-          y: _.map(activityCategoryData.values, function(activityCategoryDay) {
-            return activityCategoryDay.value[activityMetric];
-          }),
-        };
-      });
-
-      // Add plot layout configuration
-      const layout = {
-        title: TAPi18n.__('reportPageActivitiesChart-title'),
-        xaxis: {
-          title: TAPi18n.__('reportPageActivitiesChart-xaxis-title'),
-        },
-        yaxis: {
-          title: TAPi18n.__(`reportPageActivitiesChart-yaxis-${activityMetric}`),
-        },
-        barmode,
-      };
-
-      // Get client locale
-      const locale = TAPi18n.getLanguage();
-
-      // Render plot
-      Plotly.newPlot('residentsActivitiesChart', data, layout, { locale });
-    }
-  })
+  templateInstance.autorun(() => {
+    const activityDataByRole = templateInstance.activityDataByRole.get();
+    const activityDataByType = templateInstance.activityDataByType.get();
+    prepareChartData(
+      templateInstance,
+      activityDataByType,
+      'residentsActivitiesChartByType'
+    );
+    prepareChartData(
+      templateInstance,
+      activityDataByRole,
+      'residentsActivitiesChartByRole'
+    );
+  });
 });
 
 Template.report.events({
-  'change #activityMetric' (event, templateInstance) {
-    const activityMetric = $('input[name="activityMetric"]:checked').val();
+  'change #activityMetric'(event, templateInstance) {
+    const activityMetric = $(
+      'input[name="activityMetric"]:checked'
+    ).val();
 
     templateInstance.activityMetric.set(activityMetric);
   },
@@ -103,10 +94,70 @@ Template.report.events({
 Template.report.helpers({
   isGroupMode() {
     const barMode = Template.instance().barMode.get();
-    return barMode === 'group'
+    return barMode === 'group';
   },
-  lastUpdated() {
+  lastUpdatedType() {
     const instance = Template.instance();
-    return moment(instance.lastUpdated.get()).format('DD-MM-YYYY');
+    return moment(instance.lastUpdatedActivityType.get()).format(
+      'DD-MM-YYYY'
+    );
+  },
+  lastUpdatedRole() {
+    const instance = Template.instance();
+    return moment(instance.lastUpdatedActivityRole.get()).format(
+      'DD-MM-YYYY'
+    );
   },
 });
+
+function prepareChartData(templateInstance, activityData, chartName) {
+  const activityMetric = templateInstance.activityMetric.get();
+  const barmode = templateInstance.barMode.get();
+
+  // Make sure we have some activity data
+  if (activityData) {
+    // Chart data consists of multiple traces
+    const data = _.map(activityData, function(activityCategoryData) {
+      // Create a trace for each activity type
+      return {
+        type: 'bar',
+        name: activityCategoryData.key,
+        // X values are activity dates
+        x: _.map(activityCategoryData.values, function(
+          activityCategoryDay
+        ) {
+          return new Date(activityCategoryDay.key);
+        }),
+        // Y values are (currently) activity minutes
+        // TODO: add page element to toggle between activity counts and minutes
+        y: _.map(activityCategoryData.values, function(
+          activityCategoryDay
+        ) {
+          return activityCategoryDay.value[activityMetric];
+        }),
+      };
+    });
+
+    // Add plot layout configuration
+    const layout = {
+      title: TAPi18n.__('reportPageActivitiesChart-title'),
+      xaxis: {
+        title: TAPi18n.__('reportPageActivitiesChart-xaxis-title'),
+      },
+      yaxis: {
+        title: TAPi18n.__(
+          `reportPageActivitiesChart-yaxis-${activityMetric}`
+        ),
+      },
+      barmode,
+    };
+
+    // Get client locale
+    const locale = TAPi18n.getLanguage();
+
+    // Render plot
+    Plotly.newPlot(chartName, data, layout, {
+      locale,
+    });
+  }
+}
