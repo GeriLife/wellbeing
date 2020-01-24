16990f436fb319d7895e35609a8f3538a6f40850
shailee mehta <mehtashailee21@gmail.com>
2020-01-24 23:07:38 +0530

    [512] moving cron job logic to a meteor method so that it can be used to create mock data

--- app/server/crons/aggregateActivities.js
+++ app/server/crons/aggregateActivities.js
@@ -2,47 +2,8 @@ import { CronJob } from 'cron';
 
 new CronJob(
   '0 0 12 * * *',
-  Meteor.bindEnvironment(function(err, data) {
-    /* Get monthly data */
-    Meteor.call('getAllHomeReportAggregates', function(error, data) {
-      if (error || data.error === true) return;
-
-      /* Remove previous entries, if any so that after the following insert
-        there is always only one entry in the collection */
-      AllHomesActivityReportAggregate.remove({});
-      const lastUpdatedDate = new Date();
-
-      /* Prepapre data for insert adding 2 rows as per type bcause 
-         retival will be easy
-      */
-      const activityTypeData = {
-        lastUpdatedDate,
-        weeklyData: data.weeklyDataByActivityType,
-        aggregateBy: 'type',
-        monthlyData: data.monthlyDataByActivityType,
-      };
-      const facilitatorData = {
-        lastUpdatedDate,
-        weeklyData: data.weeklyDataByActivityFacilitator,
-        aggregateBy: 'facilitator',
-        monthlyData: data.monthlyDataByActivityFacilitator,
-      };
-
-      /* Insert Data */
-      AllHomesActivityReportAggregate.insert(
-        activityTypeData,
-        function(error) {
-          if (error) console.error('activityTypeData: ', error);
-        }
-      );
-
-      AllHomesActivityReportAggregate.insert(
-        facilitatorData,
-        function(error) {
-          if (error) console.error('facilitatorData: ', error);
-        }
-      );
-    });
+  Meteor.bindEnvironment(function() {
+    Meteor.call('aggregateActivitiesAndPopulateAggregateCollection');
   }),
   null,
   true
