ab95821d180bee9aa60c9ac53a40066928a361f4
shailee mehta <mehtashailee21@gmail.com>
2020-01-25 15:28:15 +0530

    [512] using demo-environment variable to run mock data creation

--- app/start-demo.sh
+++ app/start-demo.sh
@@ -1,4 +1,5 @@
  #!/bin/bash
 export MONGO_URL="mongodb://localhost:27019/wellbeing-demo"
+export GERILIFE_DEMO=true
 mongo wellbeing-demo --port 27019 --eval "db.dropDatabase()"
 meteor --port 9000
