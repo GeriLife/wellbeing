1c263fa88db440960eafa591903d165906204223
shailee <mehtashailee21@gmail.com>
2020-02-03 00:30:11 +0530

    [512] making changes to sh file as per cron enviroment and adding cron steps

--- app/start-demo.sh
+++ app/start-demo.sh
@@ -8,3 +8,11 @@ export GERILIFE_DEMO=true
 /usr/local/bin/meteor --allow-superuser reset
 
 /usr/local/bin/meteor --allow-superuser --port 9000
+
+# Steps to start cron job
+## Make sh file executable
+## sudo chmod +x start-demo.sh
+## Make current user owner of meteor file
+## sudo chown -Rh shailee .meteor/local
+# Run cron job
+## 0 0 * * * cd /path/to/sh-file && /bin/sh start-demo.sh >> cron.log 2>&1
