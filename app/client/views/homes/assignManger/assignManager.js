4854654f5e1d44622b27b8c587b2267291e1c245
Shailee Mehta <mehtashailee21@gmail.com>
2020-02-29 15:50:40 +0530

    508 adding formatting

--- app/client/views/homes/assignManger/assignManager.js
+++ app/client/views/homes/assignManger/assignManager.js
@@ -77,7 +77,9 @@ Template.assignManager.events({
       if (err || data == 0) {
         /* Set error message */
         const message =
-          err && err.message ? err.message : TAPi18n.__('couldNotRevokeAccess');
+          err && err.message
+            ? err.message
+            : TAPi18n.__('couldNotRevokeAccess');
         FlashMessages.sendError(message, { autoHide: true, hideDelay: 3000 });
       } else {
         FlashMessages.sendSuccess(successMessage, {
