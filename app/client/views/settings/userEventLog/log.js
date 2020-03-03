278bff076e3f2451912d56643d1fb9af4d5e3e7f
Shailee Mehta <mehtashailee21@gmail.com>
2020-03-03 23:19:40 +0530

    508 code climate issue

--- app/client/views/settings/userEventLog/log.js
+++ app/client/views/settings/userEventLog/log.js
@@ -1,32 +1,34 @@
 import UserEventLog from '/both/collections/userEventLog';
 
+const fields = [
+  {
+    key: 'eventDate',
+    label: TAPi18n.__('userEventLog.eventDate'),
+  },
+  {
+    key: 'userId',
+    label: TAPi18n.__('userEventLog.userId'),
+  },
+  {
+    key: 'action',
+    label: TAPi18n.__('userEventLog.action'),
+  },
+  {
+    key: 'entityType',
+    label: TAPi18n.__('userEventLog.entityType'),
+  },
+  {
+    key: 'entityId',
+    label: TAPi18n.__('userEventLog.entityId'),
+  },
+];
+
 Template.userEventLog.helpers({
   tableSettings() {
     return {
       rowsPerPage: 10,
       showFilter: false,
-      fields: [
-        {
-          key: 'eventDate',
-          label: TAPi18n.__('userEventLog.eventDate'),
-        },
-        {
-          key: 'userId',
-          label: TAPi18n.__('userEventLog.userId'),
-        },
-        {
-          key: 'action',
-          label: TAPi18n.__('userEventLog.action'),
-        },
-        {
-          key: 'entityType',
-          label: TAPi18n.__('userEventLog.entityType'),
-        },
-        {
-          key: 'entityId',
-          label: TAPi18n.__('userEventLog.entityId'),
-        },
-      ],
+      fields,
     };
   },
 });
