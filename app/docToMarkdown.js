586566dda8f51727484dba852e4480837eccea40
Shailee Mehta <mehtashailee21@gmail.com>
2020-05-06 22:21:46 +0530

    538 adding method call to generate docs so it does not conflict with app execution

--- app/docToMarkdown.js
+++ app/docToMarkdown.js
@@ -1,6 +1,10 @@
-const jsdoc2md = require('jsdoc-to-markdown');
-const fs = require('fs');
+module.exports.init = function () {
+  const jsdoc2md = require('jsdoc-to-markdown');
+  const fs = require('fs');
 
-jsdoc2md.render({ files: './server/**/*.js' }).then(function (data) {
-  fs.writeFileSync('docs.md', data, { encoding: 'utf8' });
-});
+  jsdoc2md
+    .render({ files: './server/**/*.js' })
+    .then(function (data) {
+      fs.writeFileSync('docs.md', data, { encoding: 'utf8' });
+    });
+};
