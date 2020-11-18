53cf24bfc25ef0cfab8b07166680ffd3ad59aeef
Shailee Mehta <mehtashailee21@gmail.com>
2020-11-18 17:34:01 +0530

    562 changing usage of d3 nest after version upgrade

--- app/server/methods/feelings.js
+++ app/server/methods/feelings.js
@@ -6,34 +6,45 @@ import d3 from 'd3';
 import { isCurrentUserAdmin } from '../utils/user';
 
 Meteor.methods({
-  getFeelingsCountsByResidentId (residentId) {
+  getFeelingsCountsByResidentId(residentId) {
     // Get all feelings observations for resident
     const residentFeelings = Feelings.find({ residentId }).fetch();
 
     // Get counts of feelings grouped by type
-    return d3.nest()
-      // each resident feeling observation has a 'feeling' field
-      // group observations by value of 'feeling'
-      .key(function (residentFeeling) { return residentFeeling.feeling })
-      // count number of observations for each feeling type
-      .rollup(function (feelingsArray) { return feelingsArray.length })
-      // pass resident feelings into D3 nest/rollup
-      .entries(residentFeelings);
-
+    // each resident feeling observation has a 'feeling' field
+    // group observations by value of 'feeling'
+    const grouped = d3.groups(residentFeelings, function (
+      residentFeeling
+    ) {
+      return residentFeeling.feeling;
+    });
+    // count number of observations for each feeling type
+    return grouped.map(function (feelingsArray) {
+      return {
+        key: feelingsArray[0],
+        value: feelingsArray[1].length,
+      };
+    });
   },
-  getFeelingsPercentagesByResidentId (residentId) {
+
+  getFeelingsPercentagesByResidentId(residentId) {
     // Get all feelings observations for resident
     const residentFeelings = Feelings.find({ residentId }).fetch();
 
     // Get counts of feelings grouped by type
-    return d3.nest()
-      // each resident feeling observation has a 'feeling' field
-      // group observations by value of 'feeling'
-      .key(function (residentFeeling) { return residentFeeling.feeling })
-      // count percentage of observations for each feeling type
-      .rollup(function (aggregateFeelings) { return aggregateFeelings.length / residentFeelings.length })
-      // pass resident feelings into D3 nest/rollup
-      .entries(residentFeelings);
+    // each resident feeling observation has a 'feeling' field
+    // group observations by value of 'feeling'
+    const grouped = d3.groups(residentFeelings, function (
+      residentFeeling
+    ) {
+      return residentFeeling.feeling;
+    });
+    // count percentage of observations for each feeling type
+    return grouped.map(function (aggregateFeelings) {
+      return {
+        key: aggregateFeelings[0],
+        value: aggregateFeelings[1].length / residentFeelings.length};
+    });
   },
 
   addFeeling(feelingData){
