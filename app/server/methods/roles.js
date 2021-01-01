97fa9d0631f4e43b49bcc649a6d61951afe2f33f
Shailee Mehta <mehtashailee21@gmail.com>
2021-01-01 16:11:06 +0530

    adding api related changes for settings pages

--- app/server/methods/roles.js
+++ app/server/methods/roles.js
@@ -2,7 +2,7 @@ import { isCurrentUserAdmin } from '../utils/user';
 
 Meteor.methods({
   addRole(roleName) {
-    if (!isCurrentUserAdmin()) {
+    if (!isCurrentUserAdmin(this.userId)) {
       throw new Meteor.Error(500, 'Operation not allowed');
     }
     return Meteor.roles.insert(roleName, { filter: false });
